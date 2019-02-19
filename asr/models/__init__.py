4e9cab2b37150addd14716664dcfc3096c8bf7b2
Jonathan de Bruin <jonathandebruinos@gmail.com>
2019-02-19 14:37:49 +0100

    Fix broken import path

--- asr/models/__init__.py
+++ asr/models/__init__.py
@@ -1,2 +1,2 @@
 from asr.models.lstm import create_lstm_model
-from asr.sklearn_models import create_nb_model, create_svc_model
+from asr.models.sklearn_models import create_nb_model, create_svc_model
