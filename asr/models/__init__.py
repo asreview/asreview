30e0d8a3946cda30f1a1d7ae89ac5c604396a233
Raoul Schram <r.d.schram@uu.nl>
2019-03-27 13:28:00 +0100

    So many changes...
    * More logging (settings, time).
    * Add new option for a configuration file.
    * More LSTM options.
    ** Enabled recurrent dropout for LSTM

--- asr/models/__init__.py
+++ asr/models/__init__.py
@@ -1,2 +1,3 @@
-from asr.models.lstm import create_lstm_model
+from asr.models.lstm import create_lstm_model, lstm_model_defaults
+from asr.models.lstm import lstm_fit_defaults
 from asr.models.sklearn_models import create_nb_model, create_svc_model
