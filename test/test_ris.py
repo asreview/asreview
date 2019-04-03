283a15cb22476303285f17cbae698dc1138b998d
Jonathan de Bruin <jonathandebruinos@gmail.com>
2019-04-03 10:53:30 +0200

    Add unit tests

---
+++ test/test_ris.py
@@ -0,0 +1,23 @@
+
+from pathlib import Path
+
+import asr
+
+
+def test_ris_reader_without_labels():
+
+    fp = Path("test", "demo_data", "ris_example_without_labels.ris")
+    data = asr.read_ris(fp)
+
+    assert len(data) == 2
+
+
+def test_ris_reader_with_labels():
+
+    fp = Path("test", "demo_data", "ris_example_with_labels.ris")
+    data = asr.read_ris(fp, labels=True)
+
+    assert len(data) == 2
+
+    assert data[0]['label_included'] == '1'
+    assert data[1]['label_included'] == '0'
