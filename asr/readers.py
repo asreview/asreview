399cb1b4b6cb02c3878c5dbcf6aecf1ae952f539
Jonathan de Bruin <jonathandebruinos@gmail.com>
2019-04-03 10:52:59 +0200

    Remove load_data from this file

--- asr/readers.py
+++ asr/readers.py
@@ -4,36 +4,6 @@
 # external dependencies
 from RISparser import TAG_KEY_MAPPING, readris
 
-import pandas as pd
-
-
-def load_data(fp):
-    """Load papers and their labels.
-
-    Arguments
-    ---------
-    fp: str
-        File path to the data.
-
-    Returns
-    -------
-    np.ndarray, np.array
-        The title and abstract merged into a single string for each paper.
-        The labels for each paper. 1 is included, 0 is excluded. If this column
-        is not available, this column is not returned.
-    """
-
-    df = pd.read_csv(fp)
-
-    # make texts and labels
-    texts = (df['title'].fillna('') + ' ' + df['abstract'].fillna(''))
-
-    try:
-        labels = df["included_final"]
-    except KeyError:
-        return texts.values
-
-    return texts.values, labels.values
 
 
 def read_ris(fp, label=None):
