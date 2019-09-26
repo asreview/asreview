3cd040d8e4c2e733d0c6fc9b226bb1564c41670f
Raoul Schram <r.d.schram@uu.nl>
2019-09-26 14:37:42 +0200

    Avoid import nightmare

--- asreview/balance_strategies/base.py
+++ asreview/balance_strategies/base.py
@@ -1,30 +1,7 @@
 from abc import ABC
 from abc import abstractmethod
 
-from asreview.balance_strategies.full_sampling import FullSampleTD
-from asreview.balance_strategies.triple_balance import TripleBalanceTD
-from asreview.balance_strategies.undersampling import UndersampleTD
 from asreview.utils import _unsafe_dict_update
-
-
-def get_balance_strategy(settings):
-    """ Function to get data rebalancing method. """
-    method = getattr(settings, "balance_strategy", "simple")
-    settings.balance_strategy = method
-    if method == "simple":
-        td_obj = FullSampleTD(settings.balance_param)
-        td_string = "all training data"
-    elif method == "triple_balance":
-        td_obj = TripleBalanceTD(
-            settings.balance_param, settings.fit_kwargs, settings.query_kwargs)
-        td_string = "triple balanced (max,rand) training data"
-    elif method in ["undersample", "undersampling"]:
-        td_obj = UndersampleTD(**settings.balance_param)
-        td_string = "undersampled training data"
-    else:
-        raise ValueError(f"Training data method {method} not found")
-    func, settings.balance_kwargs = td_obj.func_kwargs()
-    return func, td_string
 
 
 class BaseTrainData(ABC):
