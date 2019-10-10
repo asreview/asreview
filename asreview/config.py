d3153b93a72ffd0d3a975694b83bf574a88f820a
Raoul Schram <r.d.schram@uu.nl>
2019-10-10 14:55:31 +0200

    Add more demo datasets.
    Cohen link is broken until the pull request is merged.

--- asreview/config.py
+++ asreview/config.py
@@ -4,8 +4,11 @@ AVAILABLE_CLI_MODI = ["oracle", "simulate"]
 AVAILABLE_REVIEW_CLASSES = ["oracle", "simulate", "minimal"]
 
 DEMO_DATASETS = {
-    "example_ptsd": "https://raw.githubusercontent.com/msdslab/automated-systematic-review-datasets/master/datasets/Van_de_Schoot_PTSD/output/PTSD_VandeSchoot_18.csv"  # noqa
+    "example_ptsd": "https://raw.githubusercontent.com/msdslab/automated-systematic-review-datasets/master/datasets/Van_de_Schoot_PTSD/output/PTSD_VandeSchoot_18.csv",  # noqa
+    "example_hall": "https://raw.githubusercontent.com/msdslab/automated-systematic-review-datasets/master/datasets/Four%20Software%20Engineer%20Data%20Sets/Software%20Engineering%201%20Hall.csv",  # noqa
+    "example_cohen": "https://raw.githubusercontent.com/msdslab/automated-systematic-review-datasets/master/datasets/Cohen_EMB/cleaned/ACEInhibitors.csv",  # noqa
 }
+
 
 NOT_AVAILABLE = np.nan
 
