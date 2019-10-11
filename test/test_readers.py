a55a99d51ee7ae8634f88461b483240d58a7495f
qubixes <44498096+qubixes@users.noreply.github.com>
2019-10-11 11:31:10 +0200

    Fix the RIS reader + add RIS export function #73 #62 (#109)
    Closes #73 and #62.

--- test/test_readers.py
+++ test/test_readers.py
@@ -18,7 +18,7 @@ def test_csv_reader_without_labels():
 def test_csv_reader_with_labels():
 
     fp = Path("test", "demo_data", "csv_example_with_labels.csv")
-    data = asr.read_csv(fp, labels=True)
+    data = asr.read_csv(fp)
 
     assert len(data) == 6
 
@@ -50,15 +50,26 @@ def test_ris_reader_without_labels():
     assert 'label_included' not in data[0].keys()
 
 
+def check_as_data(as_data):
+    assert as_data.raw_df.shape[0] == 2
+    cols = ['labels', 'title', 'abstract', 'keywords', 'article_id']
+    print(as_data.labels)
+    for col in cols:
+        print(col)
+        assert len(getattr(as_data, col)) == 2
+    assert as_data.labels[0] == 1
+    assert as_data.labels[1] == 0
+
+
 def test_ris_reader_with_labels():
 
     fp = Path("test", "demo_data", "ris_example_with_labels.ris")
-    data = asr.read_ris(fp, labels=True)
+    test_fp = Path("test", "demo_data", "export_test.ris")
+    as_data = asr.ASReviewData.from_file(fp)
+    check_as_data(as_data)
 
-    assert len(data) == 2
-
-    assert data[0]['label_included'] == '1'
-    assert data[1]['label_included'] == '0'
+    as_data.to_ris(test_fp)
+    check_as_data(as_data)
 
 
 def test_ris_load_data():
