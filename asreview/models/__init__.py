80bd4c0d776d88c4afd72ef7fa267e9794dca859
Jonathan de Bruin <jonathandebruinos@gmail.com>
2019-11-11 15:41:24 +0100

    Change MIT license into Apache 2.0 license

--- asreview/models/__init__.py
+++ asreview/models/__init__.py
@@ -1,3 +1,17 @@
+# Copyright 2019 The ASReview Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from asreview.models.lstm_base import create_lstm_base_model, LSTMBaseModel
 from asreview.models.lstm_pool import create_lstm_pool_model, LSTMPoolModel
 from asreview.models.sklearn_models import create_nb_model, NBModel
