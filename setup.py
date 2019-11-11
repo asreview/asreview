4df69494ecb2e2e37d0423879a7ed2eb7f95b56b
Jonathan de Bruin <jonathandebruinos@gmail.com>
2019-11-11 15:41:24 +0100

    Change MIT license into Apache 2.0 license

--- setup.py
+++ setup.py
@@ -1,5 +1,18 @@
-# based on https://github.com/pypa/sampleproject
-# MIT License
+# Copyright 2019 The ASReview Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# based on https://github.com/pypa/sampleproject - MIT License
 
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
@@ -27,7 +40,7 @@ setup(
     author_email='asreview@uu.nl',
     classifiers=[
         'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: Apache Software License (Apache 2.0)',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
