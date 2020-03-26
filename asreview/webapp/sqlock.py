db9975d7d0ef90eb0a9db0f2987820503680a7c2
Raoul Schram <r.d.schram@uu.nl>
2020-03-26 13:51:31 +0100

    Fix lock

--- asreview/webapp/sqlock.py
+++ asreview/webapp/sqlock.py
@@ -18,34 +18,43 @@ def release_all_locks(db_file):
 
 
 class SQLiteLock():
-    def __init__(self, db_file, *args, lock_name="global", **kwargs):
+    def __init__(self, db_file, lock_name="global", blocking=False,
+                 timeout=30, polling_rate=0.4):
         self.db_file = db_file
         self.lock_name = lock_name
-        self.acquire(*args, **kwargs)
-
-    def acquire(self, blocking=False):
         self.lock_acquired = False
+        self.acquire(blocking=blocking, timeout=timeout,
+                     polling_rate=polling_rate)
+
+    def acquire(self, blocking=False, timeout=30, polling_rate=0.4):
+        if self.lock_acquired:
+            return
+
         if not os.path.isfile(self.db_file):
             self.init_db()
 
-        while True and not self.lock_acquired:
+        cur_timeout = 0
+        while cur_timeout < timeout and not self.lock_acquired:
             db = get_db(self.db_file)
-            db.isolation_level = 'EXCLUSIVE'
-            db.execute('BEGIN EXCLUSIVE')
-            lock_entry = db.execute(
-                'SELECT * FROM locks WHERE name = ?',
-                (self.lock_name,)).fetchone()
-            if lock_entry is None:
-                db.execute(
-                    'INSERT INTO locks (name) VALUES (?)',
-                    (self.lock_name,))
-                self.lock_acquired = True
-            db.commit()
+            try:
+                db.isolation_level = 'EXCLUSIVE'
+                db.execute('BEGIN EXCLUSIVE')
+                lock_entry = db.execute(
+                    'SELECT * FROM locks WHERE name = ?',
+                    (self.lock_name,)).fetchone()
+                if lock_entry is None:
+                    db.execute(
+                        'INSERT INTO locks (name) VALUES (?)',
+                        (self.lock_name,))
+                    self.lock_acquired = True
+                db.commit()
+            except sqlite3.OperationalError:
+                pass
             db.close()
-            if blocking:
-                sleep(0.4)
-            else:
+            if self.lock_acquired or not blocking:
                 break
+            cur_timeout += polling_rate
+            sleep(polling_rate)
 
     def init_db(self):
         db = get_db(self.db_file)
@@ -71,3 +80,4 @@ class SQLiteLock():
             (self.lock_name,))
         db.commit()
         db.close()
+        self.lock_acquired = False
