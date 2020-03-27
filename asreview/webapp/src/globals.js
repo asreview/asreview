3ea9a068800e6ee3a100e5d375a3050ba6595bb7
Jonathan de Bruin <jonathandebruinos@gmail.com>
2020-03-27 18:38:58 +0100

    Fix typos in inline docs

--- asreview/webapp/src/globals.js
+++ asreview/webapp/src/globals.js
@@ -1,6 +1,7 @@
 
-// when you running the development server, the javascript code is always
-// pointing to localhost:5000. In all other configurations,
+// When you're running the development server, the javascript code is always
+// pointing to localhost:5000. In all other configurations, the api url point to
+// the host domain.
 export const api_url = ((window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1") && window.location.port === "3000") ? "http://localhost:5000/api/" : "/api/";
 
 export const reviewDrawerWidth = 250;
