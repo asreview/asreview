fee9d80acee756cc99f1825ff517db1be7fa2ea5
Jonathan de Bruin <jonathandebruinos@gmail.com>
2020-04-03 17:43:15 +0200

    Add progress pie chart to review drawer

--- asreview/webapp/src/SideStats/ProgressPanel.js
+++ asreview/webapp/src/SideStats/ProgressPanel.js
@@ -3,38 +3,38 @@ import {
     ListSubheader,
     ListItem,
 } from '@material-ui/core';
-// import { makeStyles } from '@material-ui/core/styles';
 
-// const useStyles = makeStyles(theme => ({
-//   root: {
-//     width: '100%',
-//   },
-//   heading: {
-//     fontSize: theme.typography.pxToRem(15),
-//     fontWeight: theme.typography.fontWeightRegular,
-//   },
-// }));
+import {
+  ProgressPieChart,
+} from '../SideStats';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  item: {
+    margin: "10px 0px 10px 0px",
+  },
+}));
 
 const ProgressPanel = (props) => {
-    // const classes = useStyles();
+    const classes = useStyles();
 
     return (
- 
-        <div>
-          <ListSubheader component="div" id="list-subheader-progress">
-            Progress
-          </ListSubheader> 
-          {/*<LinearProgress variant="determinate" value="10" color="primary" />*/}
-          <ListItem key="list-progress-total_labeled">
-            Total reviewed: {props.n_included + props.n_excluded}
-          </ListItem>
-          <ListItem key="list-progress-inclusions">
-            Relevant: {props.n_included}
-          </ListItem>
-          <ListItem key="list-progress-exclusions">
-            Irrelevent: {props.n_excluded}
-          </ListItem>
-        </div>
+
+      <div>
+        <ListSubheader component="div" id="list-subheader-progress">
+          Progress
+        </ListSubheader> 
+        {/*<LinearProgress variant="determinate" value="10" color="primary" />*/}
+
+        <ProgressPieChart
+          n_included={props.n_included}
+          n_excluded={props.n_excluded}
+        />
+        <ListItem className={classes.item} key="list-progress-total_labeled">
+          Total reviewed: {props.n_included + props.n_excluded} ({Math.round((props.n_included + props.n_excluded)/props.n_papers*10000)/100}%)
+        </ListItem>
+      </div>
 
     );
 }
