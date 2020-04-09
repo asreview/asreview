b3ab765634ca84e9dc354e3fc475f7c096c92515
Jonathan de Bruin <jonathandebruinos@gmail.com>
2020-04-09 17:32:38 +0200

    Add progress pie chart to review drawer (#214)

---
+++ asreview/webapp/src/SideStats/ProgressPieChart.js
@@ -0,0 +1,48 @@
+import React, { PureComponent } from 'react';
+import {
+  Cell, PieChart, Pie, Legend, Tooltip,
+} from 'recharts';
+
+import {reviewDrawerWidth} from '../globals.js'
+
+const data = [
+  { name: 'Relevant', value: 20 }, { name: 'Irrelevant', value: 80 }
+];
+
+const ProgressPieChart = (props) => {
+
+
+  const getData = () => {
+    return([
+      { name: 'Relevant', value: props.n_included }, { name: 'Irrelevant', value: props.n_excluded }
+    ])
+  }
+
+
+  return (
+    <PieChart
+      width={reviewDrawerWidth}
+      height={(reviewDrawerWidth/2)-40}
+    >
+      <Pie
+        isAnimationActive={false}
+        dataKey="value"
+        startAngle={180}
+        endAngle={0}
+        data={getData()}
+        cx={reviewDrawerWidth/2}
+        cy={(reviewDrawerWidth/2)-40}
+        innerRadius={0}
+        outerRadius={65}
+        fill="#8884d8"
+        label
+      >
+      <Cell fill={'#00C49F'}/>
+      <Cell fill={'#FF8042'}/>
+      </Pie>
+      <Tooltip />
+    </PieChart>
+  );
+}
+
+export default ProgressPieChart;
--- asreview/webapp/src/SideStats/index.js
+++ asreview/webapp/src/SideStats/index.js
@@ -1,2 +1,3 @@
 export { default as ProgressPanel } from './ProgressPanel'
 export { default as ProjectPanel } from './ProjectPanel'
+export { default as ProgressPieChart } from './ProgressPieChart'
