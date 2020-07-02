fbed19b52068186ab35a5e42f961ae8739bc6949
terrymyc <terrymyc@gmail.com>
2020-07-02 20:43:36 +0200

    Improve useDarkMode

--- asreview/webapp/src/useDarkMode.js
+++ asreview/webapp/src/useDarkMode.js
@@ -1,42 +1,35 @@
 import { useEffect, useState } from 'react'
-import { createMuiTheme } from '@material-ui/core'
 import brown from '@material-ui/core/colors/brown';
 
-const lightTheme = createMuiTheme({
-  palette: {
-    type: "light",
-    primary: brown,
-  },
-});
-
-const darkTheme = createMuiTheme({
-  palette: {
-    type: "dark",
-    primary: brown,
-  },
-});
 
 const useDarkMode = () => {
 
-  const [theme, setTheme] = useState(lightTheme)
+  let defaultTheme = {
+    palette: {
+      type: "light",
+      primary: brown,
+    },
+  };
+
+  const [theme, setTheme] = useState(defaultTheme)
 
   const toggleDarkMode = () => {
 
     if (theme.palette.type === "light") {
       window.localStorage.setItem("theme", "dark")
-      setTheme(darkTheme)
+      setTheme({palette: {...defaultTheme.palette, type: "dark"}})
     } else {
       window.localStorage.setItem("theme", "light")
-      setTheme(lightTheme)
+      setTheme(defaultTheme)
     }
   }
 
   useEffect(() => {
     const localTheme = window.localStorage.getItem("theme")
     if (theme.palette.type !== localTheme) {
-      setTheme(darkTheme)
+      setTheme({palette: {...defaultTheme.palette, type: "dark"}})
     }
-  }, [theme.palette.type])
+  }, [defaultTheme.palette, theme.palette.type])
 
   return [theme, toggleDarkMode]
 }
