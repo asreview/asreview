077ece61947571d2a60a96a270ee643645b42dec
Raoul Schram <r.d.schram@uu.nl>
2020-07-21 15:37:08 +0200

    Add stop words to TFIDF

--- asreview/feature_extraction/tfidf.py
+++ asreview/feature_extraction/tfidf.py
@@ -1,6 +1,4 @@
-from sklearn.pipeline import Pipeline
-from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 from asreview.feature_extraction.base import BaseFeatureExtraction
 
@@ -9,7 +7,7 @@ class Tfidf(BaseFeatureExtraction):
     """Class to apply SKLearn Tf-idf to texts."""
     name = "tfidf"
 
-    def __init__(self, *args, ngram_max=1, **kwargs):
+    def __init__(self, *args, ngram_max=1, stop_words=0, **kwargs):
         """Initialize tfidf class.
 
         Arguments
@@ -17,13 +15,18 @@ class Tfidf(BaseFeatureExtraction):
         ngram_max: int
             Can use up to ngrams up to ngram_max. For example in the case of
             ngram_max=2, monograms and bigrams could be used.
+        stop_words: int
+            Use *ENGLISH* stop words.
         """
         super(Tfidf, self).__init__(*args, **kwargs)
         self.ngram_max = ngram_max
-        self._model = Pipeline([
-            ('vect', CountVectorizer(ngram_range=(1, ngram_max))),
-            ('tfidf', TfidfTransformer())]
-        )
+        self.stop_words = stop_words
+        if stop_words == 1:
+            sklearn_stop_words = 'english'
+        else:
+            sklearn_stop_words = None
+        self._model = TfidfVectorizer(ngram_range=(1, ngram_max),
+                                      stop_words=sklearn_stop_words)
 
     def fit(self, texts):
         self._model.fit(texts)
@@ -37,6 +40,7 @@ class Tfidf(BaseFeatureExtraction):
 
         hyper_space, hyper_choices = super(Tfidf, self).full_hyper_space()
         hyper_space.update({
-            "fex_ngram_max": hp.uniformint("fex_ngram_max", 1, 3)
+            "fex_ngram_max": hp.uniformint("fex_ngram_max", 1, 3),
+            "fex_stop_words": hp.randint("fex_stop_words", 2),
         })
         return hyper_space, hyper_choices
