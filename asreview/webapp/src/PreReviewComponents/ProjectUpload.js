74cd89117ada6b1af800e08388adc93f37d05226
Jonathan de Bruin <jonathandebruinos@gmail.com>
2020-07-28 13:45:45 +0200

    Fix typo

--- asreview/webapp/src/PreReviewComponents/ProjectUpload.js
+++ asreview/webapp/src/PreReviewComponents/ProjectUpload.js
@@ -516,7 +516,7 @@ const ProjectUpload = (props) => {
           {statistics['filename']}
           </Typography>
           <Box>
-            <Typography style={{ color: green[500] }} ><CheckIcon/> Successfull upload</Typography>
+            <Typography style={{ color: green[500] }} ><CheckIcon/> Successful upload</Typography>
           </Box>
           <Typography variant="subtitle1">{statistics['n_rows']} publications</Typography>
         </CardContent>
