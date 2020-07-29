4c26d98793b2ccc635d4e06f7dcf82dc24691eea
Jonathan de Bruin <jonathandebruinos@gmail.com>
2020-07-29 21:56:06 +0200

    Fix gensim dependency (problem with security hash) (#313)

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ def get_long_description():
 
 DEPS = {
     "sbert": ['sentence_transformers'],
-    "doc2vec": ['gensim'],
+    "doc2vec": ['gensim<=3.8.2'],
     "tensorflow": ['tensorflow'],
     "dev": ['check-manifest'],
     'test': ['coverage', 'pytest'],
