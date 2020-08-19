e6eecb9441779568b156b2685e3a9f9cd6457ab3
Jonathan de Bruin <jonathandebruinos@gmail.com>
2020-08-19 16:25:21 +0200

    Change order - first is preferred

--- asreview/config.py
+++ asreview/config.py
@@ -35,9 +35,9 @@ STATE_EXTENSIONS = ['.h5', '.hdf5', '.he5', '.json']
 LOGGER_EXTENSIONS = STATE_EXTENSIONS
 
 COLUMN_DEFINITIONS = [
-    ["final_included", "label", "label_included", "included_label",
+    ["label_included", "final_included", "label", "included_label",
      "included_final", "included", "included_flag", "include"],
-    ["abstract_included", "included_abstract", "included_after_abstract", "label_abstract_screening"],
+    ["label_abstract_screening", "abstract_included", "included_abstract", "included_after_abstract"],
     ['title', 'primary_title'],
     ['authors', 'author names', 'first_authors'],
     ['abstract', 'abstract note'],
