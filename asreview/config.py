4c0c8fdb4b975c306ed2a28a26a99669b3f5979e
Jonathan de Bruin <jonathandebruinos@gmail.com>
2020-08-19 18:26:00 +0200

    Revert asreview/asreview#323 (#324)

--- asreview/config.py
+++ asreview/config.py
@@ -35,9 +35,9 @@ STATE_EXTENSIONS = ['.h5', '.hdf5', '.he5', '.json']
 LOGGER_EXTENSIONS = STATE_EXTENSIONS
 
 COLUMN_DEFINITIONS = [
-    ["label_included", "final_included", "label", "included_label",
+    ["final_included", "label", "label_included", "included_label",
      "included_final", "included", "included_flag", "include"],
-    ["label_abstract_screening", "abstract_included", "included_abstract", "included_after_abstract"],
+    ["abstract_included", "included_abstract", "included_after_abstract", "label_abstract_screening"],
     ['title', 'primary_title'],
     ['authors', 'author names', 'first_authors'],
     ['abstract', 'abstract note'],
