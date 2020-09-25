a71960f1814aafcf9f81398dd2d03cbdb0d25416
Jonathan de Bruin <jonathandebruinos@gmail.com>
2020-09-25 19:41:14 +0200

    Move boot HTTP request from API to root of Flask app (#344)

--- asreview/webapp/src/globals.js
+++ asreview/webapp/src/globals.js
@@ -2,7 +2,8 @@
 // When you're running the development server, the javascript code is always
 // pointing to localhost:5000. In all other configurations, the api url point to
 // the host domain.
-export const api_url = ((window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1") && window.location.port === "3000") ? "http://localhost:5000/api/" : "/api/";
+export const base_url = ((window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1") && window.location.port === "3000") ? "http://localhost:5000/" : "/";
+export const api_url = base_url + "api/";
 
 export const donateURL = "https://asreview.nl/donate";
 
