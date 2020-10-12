ac381a82da52e0e43ca4058fbffaf643fc2f9496
GerbrichFerdinands <45358236+GerbrichFerdinands@users.noreply.github.com>
2020-10-12 11:49:33 +0200

    spelling error (#1)
    I think inbalanced should be imbalanced.

--- asreview/balance_strategies/__init__.py
+++ asreview/balance_strategies/__init__.py
@@ -24,7 +24,7 @@ from asreview.balance_strategies.utils import list_balance_strategies
 
 There are several balance strategies that rebalance and reorder the
 training data. This is sometimes necessary, because the data is often
-very inbalanced: there are many more papers that should be excluded than
+very imbalanced: there are many more papers that should be excluded than
 included (otherwise, automation cannot help much anyway).
 
 There are several balance strategies available. In configuration
