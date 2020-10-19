da36a1699f31d393d83a503f4297c25082ac700f
Jonathan de Bruin <jonathandebruinos@gmail.com>
2020-10-19 22:44:16 +0200

    Merge files of entry points to improve maintainability (#383)

--- asreview/entry_points/run_model.py
+++
@@ -1,9 +0,0 @@
-from asreview.entry_points.base import BaseEntryPoint
-from asreview.webapp.run_model import main
-
-
-class WebRunModelEntryPoint(BaseEntryPoint):
-    description = "Internal use only."
-
-    def execute(self, argv):
-        main(argv)
