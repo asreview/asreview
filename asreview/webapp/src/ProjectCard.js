59f34fdb74fc19ecaa9944ddc5c3cc801a63a660
Jonathan de Bruin <jonathandebruinos@gmail.com>
2020-11-10 09:48:14 +0100

    Remove linting errors

--- asreview/webapp/src/ProjectCard.js
+++ asreview/webapp/src/ProjectCard.js
@@ -2,9 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 import store from './redux/store'
