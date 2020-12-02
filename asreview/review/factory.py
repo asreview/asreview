36014284696fbc1dacb54ea1af48ad08f585a251
Jonathan de Bruin <jonathandebruinos@gmail.com>
2020-12-02 13:38:45 +0100

    Revert "Force to use index"
    This reverts commit bbfce34d19046a7fc5fff7619496d045a6ae517f.

--- asreview/review/factory.py
+++ asreview/review/factory.py
@@ -262,10 +262,7 @@ def review(*args,
     # output the prior indices
     print("The following records are prior knowledge:\n")
     for prior_record_id in reviewer.start_idx:
-        preview = preview_record(reviewer.as_data.record(
-            prior_record_id,
-            by_index=True
-        ))
+        preview = preview_record(reviewer.as_data.record(prior_record_id))
         print(f"{prior_record_id} - {preview}")
 
     # Start the review process.
