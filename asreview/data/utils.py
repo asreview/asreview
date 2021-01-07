0aff678d3f1789d8e55c3d881ed847ee040f5684
Jonathan de Bruin <jonathandebruinos@gmail.com>
2021-01-07 21:30:56 +0100

    Add describe functions

---
+++ asreview/data/utils.py
@@ -0,0 +1,87 @@
+# Copyright 2019-2020 The ASReview Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from asreview.data.statistics import n_records
+from asreview.data.statistics import n_included
+from asreview.data.statistics import n_excluded
+from asreview.data.statistics import n_unlabeled
+from asreview.data.statistics import n_missing_title
+from asreview.data.statistics import n_missing_abstract
+from asreview.data.statistics import title_length
+from asreview.data.statistics import abstract_length
+from asreview.data.statistics import n_keywords
+
+
+def describe_data(data):
+    """Compute dataset statisticsg.
+
+    Returns
+    -------
+    dict:
+        Output the statistics to a dict.
+    """
+    _n_missing_title, _n_missing_title_included = n_missing_title(data)
+    _n_missing_abs, _n_missing_abs_included = n_missing_abstract(data)
+
+    return {
+        "n_records": n_records(data),
+        "n_included": n_included(data),
+        "n_excluded": n_excluded(data),
+        "n_unlabeled": n_unlabeled(data),
+        "n_missing_title": _n_missing_title,
+        "n_missing_title_included": _n_missing_title_included,
+        "n_missing_abstract": _n_missing_abs,
+        "n_missing_abstract_included": _n_missing_abs_included,
+        "title_length": title_length(data),
+        "abstract_length": abstract_length(data),
+        "n_keywords": n_keywords(data),
+    }
+
+
+def format_describe_data(data):
+    """Format the dataset statistics to string.
+
+    Returns
+    -------
+    str:
+        Output with datasets to string.
+    """
+    values = describe_data(data)
+    if values['n_keywords'] is not None:
+        avg_keywords = f"{values['n_keywords']:.1f}"
+    else:
+        avg_keywords = None
+    values_str = (
+        f"Number of papers:            {values['n_records']}\n"
+        f"Number of inclusions:        {values['n_included']} "
+        f"({100*values['n_included']/values['n_records']:.2f}%)\n"
+        f"Number of exclusions:        {values['n_excluded']} "
+        f"({100*values['n_excluded']/values['n_records']:.2f}%)\n"
+        f"Number of unlabeled:         {values['n_unlabeled']} "
+        f"({100*values['n_unlabeled']/values['n_records']:.2f}%)\n"
+        f"Average title length:        {values['title_length']:.0f}\n"
+        f"Average abstract length:     {values['abstract_length']:.0f}\n"
+        f"Average number of keywords:  {avg_keywords}\n"
+        f"Number of missing titles:    {values['n_missing_title']}"
+    )
+    if values['n_missing_title_included'] is not None:
+        values_str += (f" (of which {values['n_missing_title_included']}"
+                       " included)")
+    values_str += (f"\nNumber of missing abstracts: "
+                   f"{values['n_missing_abstract']}")
+    if values['n_missing_abstract_included'] is not None:
+        val = values['n_missing_abstract_included']
+        values_str += (f" (of which {val} included)")
+    values_str += "\n"
+    return values_str
