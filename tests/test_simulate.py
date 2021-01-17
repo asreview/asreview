85cb6b28e9dd79fb1ed54540885578e5ab454b13
Jonathan de Bruin <jonathandebruinos@gmail.com>
2021-01-18 00:01:42 +0100

    Use uppercase global variables for testing

--- tests/test_simulate.py
+++ tests/test_simulate.py
@@ -17,15 +17,15 @@ try:
 except ImportError:
     pass
 
-data_fp = Path("tests", "demo_data", "generic_labels.csv")
-data_fp_no_abs = Path("tests", "demo_data", "generic_labels_no_abs.csv")
-data_fp_no_title = Path("tests", "demo_data", "generic_labels_no_title.csv")
-data_fp_partial = Path("tests", "demo_data", "generic_partial_labels.csv")
-embedding_fp = Path("tests", "demo_data", "generic.vec")
-cfg_dir = Path("tests", "cfg_files")
-state_dir = Path("tests", "state_files")
-h5_state_file = Path(state_dir, "test.h5")
-json_state_file = Path(state_dir, "test.json")
+DATA_FP = Path("tests", "demo_data", "generic_labels.csv")
+DATA_FP_NO_ABS = Path("tests", "demo_data", "generic_labels_no_abs.csv")
+DATA_FP_NO_TITLE = Path("tests", "demo_data", "generic_labels_no_title.csv")
+DATA_FP_PARTIAL = Path("tests", "demo_data", "generic_partial_labels.csv")
+EMBEDDING_FP = Path("tests", "demo_data", "generic.vec")
+CFG_DIR = Path("tests", "cfg_files")
+STATE_DIR = Path("tests", "state_files")
+H5_STATE_FILE = Path(STATE_DIR, "test.h5")
+JSON_STATE_FILE = Path(STATE_DIR, "test.json")
 
 
 @pytest.mark.xfail(
@@ -39,13 +39,13 @@ def test_dataset_not_found():
 
 def test_state_continue_json(tmpdir):
 
-    inter_file = Path(state_dir, "test_1_inst.json")
+    inter_file = Path(STATE_DIR, "test_1_inst.json")
 
     if not inter_file.is_file():
-        reviewer = get_reviewer(data_fp,
+        reviewer = get_reviewer(DATA_FP,
                                 mode="simulate",
                                 model="nb",
-                                embedding_fp=embedding_fp,
+                                embedding_fp=EMBEDDING_FP,
                                 prior_idx=[1, 2, 3, 4],
                                 state_file=inter_file,
                                 n_instances=1,
@@ -65,13 +65,13 @@ def test_state_continue_json(tmpdir):
 
 def test_state_continue_h5(tmpdir):
 
-    inter_file = Path(state_dir, "test_1_inst.h5")
+    inter_file = Path(STATE_DIR, "test_1_inst.h5")
 
     if not inter_file.is_file():
-        reviewer = get_reviewer(data_fp,
+        reviewer = get_reviewer(DATA_FP,
                                 mode="simulate",
                                 model="nb",
-                                embedding_fp=embedding_fp,
+                                embedding_fp=EMBEDDING_FP,
                                 prior_idx=[1, 2, 3, 4],
                                 state_file=inter_file,
                                 n_instances=1,
@@ -102,26 +102,26 @@ def test_svm(tmpdir):
 
     # copy state file to tmp dir for changes
     tmp_json_state_fp = Path(tmpdir, "tmp_state.json")
-    copyfile(json_state_file, tmp_json_state_fp)
+    copyfile(JSON_STATE_FILE, tmp_json_state_fp)
 
     check_model(model="svm",
                 state_file=tmp_json_state_fp,
                 n_instances=1,
                 n_queries=2,
-                data_fp=data_fp_no_abs)
+                data_fp=DATA_FP_NO_ABS)
 
 
 def test_rf(tmpdir):
 
     # copy state file to tmp dir for changes
     tmp_json_state_fp = Path(tmpdir, "tmp_state.json")
-    copyfile(json_state_file, tmp_json_state_fp)
+    copyfile(JSON_STATE_FILE, tmp_json_state_fp)
 
     check_model(model="rf",
                 state_file=tmp_json_state_fp,
                 n_instances=1,
                 n_queries=2,
-                data_fp=data_fp_no_title)
+                data_fp=DATA_FP_NO_TITLE)
 
 
 @pytest.mark.xfail(not ADVANCED_DEPS["tensorflow"],
@@ -131,7 +131,7 @@ def test_nn_2_layer(tmpdir):
 
     # copy state file to tmp dir for changes
     tmp_json_state_fp = Path(tmpdir, "tmp_state.json")
-    copyfile(json_state_file, tmp_json_state_fp)
+    copyfile(JSON_STATE_FILE, tmp_json_state_fp)
 
     check_model(model="nn-2-layer",
                 state_file=tmp_json_state_fp,
@@ -146,9 +146,9 @@ def test_lstm_base(tmpdir):
 
     # copy state file to tmp dir for changes
     tmp_h5_state_fp = Path(tmpdir, "tmp_state.h5")
-    copyfile(h5_state_file, tmp_h5_state_fp)
+    copyfile(H5_STATE_FILE, tmp_h5_state_fp)
 
-    check_model(config_file=Path(cfg_dir, "lstm_base.ini"),
+    check_model(config_file=Path(CFG_DIR, "lstm_base.ini"),
                 state_file=tmp_h5_state_fp)
 
 
@@ -159,9 +159,9 @@ def test_lstm_pool(tmpdir):
 
     # copy state file to tmp dir for changes
     tmp_json_state_fp = Path(tmpdir, "tmp_state.json")
-    copyfile(json_state_file, tmp_json_state_fp)
+    copyfile(JSON_STATE_FILE, tmp_json_state_fp)
 
-    check_model(config_file=Path(cfg_dir, "lstm_pool.ini"),
+    check_model(config_file=Path(CFG_DIR, "lstm_pool.ini"),
                 state_file=tmp_json_state_fp)
 
 
@@ -169,7 +169,7 @@ def test_logistic(tmpdir):
 
     # copy state file to tmp dir for changes
     tmp_json_state_fp = Path(tmpdir, "tmp_state.json")
-    copyfile(json_state_file, tmp_json_state_fp)
+    copyfile(JSON_STATE_FILE, tmp_json_state_fp)
 
     check_model(model="logistic",
                 state_file=tmp_json_state_fp,
@@ -185,9 +185,9 @@ def test_partial_simulation(tmpdir):
 
     # copy state file to tmp dir for changes
     tmp_h5_state_fp = Path(tmpdir, "tmp_state.h5")
-    copyfile(h5_state_file, tmp_h5_state_fp)
+    copyfile(H5_STATE_FILE, tmp_h5_state_fp)
 
-    check_model(data_fp=data_fp_partial,
+    check_model(data_fp=DATA_FP_PARTIAL,
                 state_file=tmp_h5_state_fp,
                 n_prior_included=1,
                 n_prior_excluded=1,
@@ -203,9 +203,9 @@ def test_partial_simulation_2(tmpdir):
 
     # copy state file to tmp dir for changes
     tmp_h5_state_fp = Path(tmpdir, "tmp_state.h5")
-    copyfile(h5_state_file, tmp_h5_state_fp)
+    copyfile(H5_STATE_FILE, tmp_h5_state_fp)
 
-    check_model(data_fp=data_fp_partial,
+    check_model(data_fp=DATA_FP_PARTIAL,
                 state_file=tmp_h5_state_fp,
                 prior_idx=[0, 5],
                 state_checker=check_partial_state)
@@ -259,7 +259,7 @@ def check_model(monkeypatch=None,
                 state_file=None,
                 continue_from_state=False,
                 mode="simulate",
-                data_fp=data_fp,
+                data_fp=DATA_FP,
                 state_checker=check_state,
                 prior_idx=[1, 2, 3, 4],
                 **kwargs):
@@ -275,7 +275,7 @@ def check_model(monkeypatch=None,
     # start the review process.
     reviewer = get_reviewer(data_fp,
                             mode=mode,
-                            embedding_fp=embedding_fp,
+                            embedding_fp=EMBEDDING_FP,
                             prior_idx=prior_idx,
                             state_file=state_file,
                             **kwargs)
