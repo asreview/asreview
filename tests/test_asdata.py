e06a2358aca2b15a29af291127eb8dc85f527bf4
Jonathan de Bruin <jonathandebruinos@gmail.com>
2021-01-18 20:54:19 +0100

    Remove whitespace from columns

--- tests/test_asdata.py
+++ tests/test_asdata.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import numpy as np
 import pytest
+from pandas.testing import assert_frame_equal
 
 from asreview import ASReviewData
 
@@ -22,3 +23,17 @@ def test_record_id():
 
     # test is index name is record_id
     assert as_data.df.index.name == "record_id"
+
+
+def test_column_names_with_spaces():
+
+    data_fp = Path("tests", "demo_data", "generic.csv")
+    as_data = ASReviewData.from_file(data_fp)
+
+    data_fp_bad_cols = Path("tests", "demo_data", "generic_column_names_with_spaces.csv")
+    as_data_bad_cols = ASReviewData.from_file(data_fp_bad_cols)
+
+    assert_frame_equal(
+        as_data.df[["title", "abstract"]],
+        as_data_bad_cols.df[["title", "abstract"]]
+    )
