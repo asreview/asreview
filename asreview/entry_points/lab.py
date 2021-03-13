asreview/asreview

commit b26293d1465bab090a628d5f005a82846d49baf9

Fix lowercase characters in command line options (#505)

asreview/entry_points/lab.py
@@ -30,13 +30,15 @@ def _lab_parser(prog="lab"):
     )
 
     parser.add_argument(
-        "--clean_project",
+        "--clean-project",
+        dest="--clean_project",
         default=None,
         type=str,
         help="Safe cleanup of temporary files in project.")
 
     parser.add_argument(
-        "--clean_all_projects",
+        "--clean-all-projects",
+        dest="--clean_all_projects",
         action='store_true',
         help="Safe cleanup of temporary files in all projects.")
 

commit 51f403560b6cca44428ed217eb187af4faf79326

Fix broken asreview lab startup
Fixes bug introduced in #505

asreview/entry_points/lab.py
@@ -31,14 +31,15 @@ def _lab_parser(prog="lab"):
 
     parser.add_argument(
         "--clean-project",
-        dest="--clean_project",
+        dest="clean_project",
         default=None,
         type=str,
         help="Safe cleanup of temporary files in project.")
 
     parser.add_argument(
         "--clean-all-projects",
-        dest="--clean_all_projects",
+        dest="clean_all_projects",
+        default=None,
         action='store_true',
         help="Safe cleanup of temporary files in all projects.")
 

asreview/webapp/start_flask.py
@@ -129,12 +129,16 @@ def main(argv):
 
     # clean all projects
     if args.clean_all_projects:
+        print("Cleaning all project files.")
         clean_all_project_tmp_files()
+        print("Done")
         return
 
     # clean project by project_id
     if args.clean_project is not None:
+        print(f"Cleaning project file '{args.clean_project}'.")
         clean_project_tmp_files(args.clean_project)
+        print("Done")
         return
 
     # shortcuts for host and port