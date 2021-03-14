15461971dc76f308d0653fa66afbed6a2b8dcc44
Yongchao Terry Ma <terrymyc@gmail.com>
2021-03-14 20:14:31 +0100

    Introduce Javascript API for communication with server (#512)

---
+++ asreview/webapp/src/api/BaseAPI.js
@@ -0,0 +1,22 @@
+import { base_url } from "../globals.js";
+import axios from 'axios'
+
+
+class BaseAPI {
+
+    static boot = () => {
+        const url = base_url + `boot`
+        return new Promise(function(resolve, reject) {
+            axios.get(url)
+                .then((result) => {
+                    resolve(result)
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        });
+    }
+
+}
+
+export default BaseAPI
---
+++ asreview/webapp/src/api/index.js
@@ -0,0 +1,1 @@
+export { default as BaseAPI } from './BaseAPI'
