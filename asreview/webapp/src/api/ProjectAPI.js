d3985f91d94cea813b220f29c08844752964c3ea
Yongchao Ma <terrymyc@gmail.com>
2021-03-16 17:46:06 +0100

    Wrap PriorKnowledge.js api

--- asreview/webapp/src/api/ProjectAPI.js
+++ asreview/webapp/src/api/ProjectAPI.js
@@ -29,7 +29,40 @@ class ProjectAPI {
           apiErrorHandler.withRetry(error, setError);
         });
     });
-  }
+  };
+
+  static prior_stats(project_id) {
+    const url = api_url + `project/${project_id}/prior_stats`;
+    return new Promise(function(resolve, reject) {
+      axios.get(url)
+        .then((result) => {
+          resolve(result);
+        })
+        .catch((error) => {
+          console.log("Failed to load prior information");
+        });
+    });
+  };
+
+  static labelitem(project_id, body) {
+    const url = api_url + `project/${project_id}/labelitem`;
+    return new Promise(function(resolve, reject) {
+      axios.post(
+        url,
+        body,
+        {
+          headers: {
+            'Content-type': 'application/x-www-form-urlencoded',
+          }
+        })
+      .then((result) => {
+        resolve(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    });
+  };
 
   static finish(project_id) {
     const url = api_url + `project/${project_id}/finish`;
