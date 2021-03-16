481ee460bbc1addc6848fe76dfba3b1166257e61
Yongchao Ma <terrymyc@gmail.com>
2021-03-16 16:34:06 +0100

    Wrap LabeledItems api

--- asreview/webapp/src/PreReviewComponents/LabeledItems.js
+++ asreview/webapp/src/PreReviewComponents/LabeledItems.js
@@ -16,10 +16,7 @@ import {
 import DeleteIcon from '@material-ui/icons/Delete'
 
 import ErrorHandler from '../ErrorHandler';
-
-import axios from 'axios'
-
-import { api_url } from '../globals.js';
+import { ProjectAPI } from '../api/index.js';
 
 import { connect } from "react-redux";
 
@@ -93,34 +90,19 @@ const LabeledItems = (props) => {
   useEffect(() => {
 
     if (state.loading){
-      const url = api_url + `project/${props.project_id}/prior`;
+      ProjectAPI.prior(props.project_id, setError)
+        .then((result) => {
 
-      axios.get(url)
-      .then((result) => {
+          setState(s => {return({
+            ...s,
+            "data": result.data["result"],
+            "loading": false,
+          })});
 
-        setState(s => {return({
-          ...s,
-          "data": result.data["result"],
-          "loading": false,
-        })});
-
-      })
-      .catch((error) => {
-
-        if (error.response) {
-          setError({
-            'message': error.response.data.message,
-            'retry': true,
-          });
-          console.log(error.response);
-        } else {
-          setError({
-            'message': "Failed to connect to server. Please restart the software.",
-            'retry': false,
-          });
-        };
-
-      });
+        })
+        .catch((error) => {
+          // handled in api wrapper
+        });
     }
 
   }, [props.project_id, state.loading, error.message]);
--- asreview/webapp/src/api/ProjectAPI.js
+++ asreview/webapp/src/api/ProjectAPI.js
@@ -5,21 +5,8 @@ import axios from 'axios';
 
 class ProjectAPI {
 
-  static finish(project_id) {
-    const url = api_url + `project/${project_id}/finish`
-    return new Promise(function(resolve, reject) {
-      axios.get(url)
-        .then((result) => {
-          resolve(result);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    });    
-  };
-
   static info(project_id, setError) {
-    const url = api_url + `project/${project_id}/info`
+    const url = api_url + `project/${project_id}/info`;
     return new Promise(function(resolve, reject) {
       axios.get(url)
         .then((result) => {
@@ -28,7 +15,33 @@ class ProjectAPI {
         .catch((error) => {
           apiErrorHandler.withRetry(error, setError);
         });
-    });    
+    });
+  };
+
+  static prior(project_id, setError) {
+    const url = api_url + `project/${project_id}/prior`;
+    return new Promise(function(resolve, reject) {
+      axios.get(url)
+        .then((result) => {
+          resolve(result);
+        })
+        .catch((error) => {
+          apiErrorHandler.withRetry(error, setError);
+        });
+    });
+  }
+
+  static finish(project_id) {
+    const url = api_url + `project/${project_id}/finish`;
+    return new Promise(function(resolve, reject) {
+      axios.get(url)
+        .then((result) => {
+          resolve(result);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    });
   };
 
 }
