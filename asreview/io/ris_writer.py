b538561a94c43f01f6d0cf8a7065925f783c723e
ottomattas <otto.mattas@gmail.com>
2021-04-26 14:03:20 +0200

    Turn DF into a list of dictionaries and beautify

--- asreview/io/ris_writer.py
+++ asreview/io/ris_writer.py
@@ -11,111 +11,22 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-import logging
-
-import pandas as pd
 import rispy
-from rispy import TAG_KEY_MAPPING
-from rispy.config import LIST_TYPE_TAGS
 
-from asreview.config import COLUMN_DEFINITIONS
-from asreview.io.utils import standardize_dataframe
-
-
-RIS_KEY_LABEL_INCLUDED = "LI"
-
-
-def _tag_key_mapping(reverse=False):
-    # Add label_included into the specification and create reverse mapping.
-    TAG_KEY_MAPPING[RIS_KEY_LABEL_INCLUDED] = "included"
-    KEY_TAG_MAPPING = {TAG_KEY_MAPPING[key]: key for key in TAG_KEY_MAPPING}
-    for label in COLUMN_DEFINITIONS["included"]:
-        KEY_TAG_MAPPING[label] = "LI"
-    if reverse:
-        return KEY_TAG_MAPPING
-    else:
-        return TAG_KEY_MAPPING
-
-
-def read_ris(fp):
-    """RIS file reader.
+def write_ris(df, fp):
+    """RIS file writer
 
     Parameters
     ----------
+    df: pandas.Dataframe
+        Dataframe to convert and export.
     fp: str, pathlib.Path
         File path to the RIS file.
-    label: bool
-        Check for label. If None, this is automatic.
-
-    Returns
-    -------
-    pandas.DataFrame:
-        Dataframe with entries.
-
     """
+    
+    # Turn pandas DataFrame into list of dictionaries
+    d = df.T.to_dict().values()
 
-    encodings = ['ISO-8859-1', 'utf-8', 'utf-8-sig']
-    entries = None
-    for encoding in encodings:
-        try:
-            with open(fp, 'r', encoding=encoding) as bibliography_file:
-                mapping = _tag_key_mapping(reverse=False)
-                entries = list(rispy.load(bibliography_file, mapping=mapping))
-                break
-        except UnicodeDecodeError:
-            pass
-        except IOError as e:
-            logging.warning(e)
-
-    if entries is None:
-        raise ValueError("Cannot find proper encoding for data file.")
-
-    df = pd.DataFrame(entries)
-
-    def converter(x):
-        try:
-            return ", ".join(x)
-        except TypeError:
-            return ""
-
-    for tag in LIST_TYPE_TAGS:
-        key = TAG_KEY_MAPPING[tag]
-        if key in df:
-            df[key] = df[key].apply(converter)
-    return standardize_dataframe(df)
-
-
-def write_ris(df, fp):
-    """Write dataframe to RIS file.
-
-    Arguments
-    ---------
-    df: pandas.Dataframe
-        Dataframe to export.
-    fp: str
-        RIS file to export to.
-    """
-    column_names = list(df)
-    column_key = []
-    for col in column_names:
-        try:
-            rev_mapping = _tag_key_mapping(reverse=True)
-            column_key.append(rev_mapping[col])
-        except KeyError:
-            column_key.append('UK')
-            logging.info(f"Cannot find column {col} in specification.")
-
-    n_row = df.shape[0]
-
-    # According to RIS specifications, a record should begin with TY.
-    # Thus, the column id is inserted before all the other.
-    col_order = []
-    for i, key in enumerate(column_key):
-        if key == 'TY':
-            col_order.insert(0, i)
-        else:
-            col_order.append(i)
-
+    # Export the RIS file
     with open(fp, "w") as fp:
-        rispy.dump(df, fp)
+        rispy.dump(d, fp)
