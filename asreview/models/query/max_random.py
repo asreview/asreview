42c1663221f162f322dd4e41678dbbb940d44037
Yongchao Ma <terrymyc@gmail.com>
2021-06-04 14:14:37 +0200

    Remove comment lines

--- asreview/models/query/max_random.py
+++ asreview/models/query/max_random.py
@@ -11,8 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Max sampling while saving prediction probabilities."""
-
 
 from asreview.models.query.mixed import MixedQuery
 
