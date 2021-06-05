69812af0a68c04e90364fd197a2941aa35f75316
Yongchao Ma <terrymyc@gmail.com>
2021-06-05 16:51:06 +0200

    Add pagination

--- asreview/webapp/src/ProjectTable.js
+++ asreview/webapp/src/ProjectTable.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Box,
   Chip,
@@ -8,6 +8,7 @@ import {
   TableCell,
   TableContainer,
   TableHead,
+  TablePagination,
   TableRow
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
@@ -17,14 +18,14 @@ import { setProject } from "./redux/actions";
 import { setupColor, inReviewColor, finishedColor } from "./globals";
 
 const columns = [
-  { id: "name", label: "Project", minWidth: 170 },
-  { id: "datetimeCreated", label: "Date", minWidth: 100 },
-  { id: "reviewFinished", label: "Status", minWidth: 100 },
+  { id: "name", label: "Project", width: "60%", minWidth: 400 },
+  { id: "datetimeCreated", label: "Date", width: "20%", minWidth: 100 },
+  { id: "reviewFinished", label: "Status", width: "20%", minWidth: 100 },
 ];
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    width: '100%',
+    width: "100%",
   },
   table: {
     minWidth: 700,
@@ -53,6 +54,18 @@ const ProjectTable = (props) => {
   
   const classes = useStyles();
 
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
   const formatDate = (datetime) => {
     let date = new Date(datetime);
     let dateString = date.toDateString().slice(4)
@@ -60,61 +73,72 @@ const ProjectTable = (props) => {
   };
 
   return (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} stickyHeader aria-label="sticky table">
-        <TableHead>
-          <TableRow>
-            {columns.map((column) => (
-              <TableCell
-                key={column.id}
-                style={{ minWidth: column.minWidth }}
-              >
-                {column.label}
-              </TableCell>
-            ))}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {props.projects["projects"].map((row) => {
-            const openExistingProject = () => {
-              console.log("Opening existing project " + row.id);
-
-              // set the state in the redux store
-              store.dispatch(setProject(row.id));
-
-              props.handleAppState("project-page");
-            };
-            return (
-              <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
+    <Paper className={classes.root}>
+      <TableContainer>
+        <Table className={classes.table} stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
                 <TableCell
-                  className={classes.tableCell}
+                  key={column.id}
+                  style={{ width: column.width, minWidth: column.minWidth }}
                 >
-                  <Box
-                    onClick={openExistingProject}
-                    style={{ cursor: "pointer" }}
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {props.projects["projects"].slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
+              const openExistingProject = () => {
+                console.log("Opening existing project " + row.id);
+
+                // set the state in the redux store
+                store.dispatch(setProject(row.id));
+
+                props.handleAppState("project-page");
+              };
+              return (
+                <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
+                  <TableCell
+                    className={classes.tableCell}
                   >
-                    {row["name"]}
-                  </Box>
-                </TableCell>
-                <TableCell
-                  className={classes.tableCell}
-                >
-                  {row["datetimeCreated"] ? formatDate(row["datetimeCreated"]) : "N/A"}
-                </TableCell>
-                <TableCell
-                  className={classes.tableCell}
-                >
-                  <Chip
-                    className={row["projectInitReady"] ? (row["reviewFinished"] ? classes.chipFinished : classes.chipInReview) : classes.chipSetup}
-                    label={row["projectInitReady"] ? (row["reviewFinished"] ? "FINISHED" : "IN REVIEW") : "SETUP"}
-                  />
-                </TableCell>
-              </TableRow>
-            );
-          })}
-        </TableBody>
-      </Table>
-    </TableContainer>
+                    <Box
+                      onClick={openExistingProject}
+                      style={{ cursor: "pointer" }}
+                    >
+                      {row["name"]}
+                    </Box>
+                  </TableCell>
+                  <TableCell
+                    className={classes.tableCell}
+                  >
+                    {row["datetimeCreated"] ? formatDate(row["datetimeCreated"]) : "N/A"}
+                  </TableCell>
+                  <TableCell
+                    className={classes.tableCell}
+                  >
+                    <Chip
+                      className={row["projectInitReady"] ? (row["reviewFinished"] ? classes.chipFinished : classes.chipInReview) : classes.chipSetup}
+                      label={row["projectInitReady"] ? (row["reviewFinished"] ? "FINISHED" : "IN REVIEW") : "SETUP"}
+                    />
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, props.projects["projects"].length]}
+        component="div"
+        count={props.projects["projects"].length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onChangePage={handleChangePage}
+        onChangeRowsPerPage={handleChangeRowsPerPage}
+      />
+    </Paper>
   )
 };
 
