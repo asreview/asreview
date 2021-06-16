a4aeb0fd3998d7306f475dc38d711fe8f23fb5cb
valmelnikov <mrzeliboba@gmail.com>
2021-06-16 13:45:04 +0200

    Fixed auth entry point linting

--- asreview/entry_points/auth.py
+++ asreview/entry_points/auth.py
@@ -2,48 +2,50 @@ from argparse import ArgumentParser
 
 from asreview.entry_points.base import BaseEntryPoint
 
+
 def _auth_parser(prog="auth"):
     parser = ArgumentParser(
         prog=prog,
-        description="""ASReview LAB - Active learning for Systematic Reviews. Authentication management util."""
+        description='ASReview LAB - Active learning for Systematic Reviews. '
+                    'Authentication management util.'
     )
 
-    parser.add_argument("-f", 
-        "--file", 
-        help="Path to the auth file", 
-        required=False
-    )
+    parser.add_argument("-f",
+                        "--file",
+                        help="Path to the auth file",
+                        required=False
+                        )
 
-    parser.add_argument("-u", 
-        "--username", 
-        help="Username", 
-        required=True
-    )
+    parser.add_argument("-u",
+                        "--username",
+                        help="Username",
+                        required=True
+                        )
 
-    parser.add_argument("-p", 
-        "--password", 
-        help="Password"
-    )
+    parser.add_argument("-p",
+                        "--password",
+                        help="Password"
+                        )
 
-    parser.add_argument("-r", 
-        "--remove", 
-        help="Remove user", 
-        action='store_true'
-    )
+    parser.add_argument("-r",
+                        "--remove",
+                        help="Remove user",
+                        action='store_true'
+                        )
 
-    parser.add_argument("-s", 
-        "--saltlen", 
-        help="Length of the salt string", 
-        type=int, 
-        default=64
-    )
+    parser.add_argument("-s",
+                        "--saltlen",
+                        help="Length of the salt string",
+                        type=int,
+                        default=64
+                        )
 
-    parser.add_argument("-i", 
-        "--iterhash", 
-        help="Number of hashing iterations", 
-        type=int, 
-        default=150000
-    )
+    parser.add_argument("-i",
+                        "--iterhash",
+                        help="Number of hashing iterations",
+                        type=int,
+                        default=150000
+                        )
 
     return parser
 
