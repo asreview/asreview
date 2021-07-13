asreview/asreview

commit 520caff6f38f353ba349aae19522cc0b4c5cc644

Add doi to config column definitions (#631)

asreview/io/paper_record.py
@@ -131,7 +131,7 @@ class PaperRecord():
     def __init__(self, record_id, column_spec={}, **kwargs):
 
         for attr in [
-                "title", "abstract", "authors", "keywords", "included"
+                "title", "abstract", "authors", "keywords", "doi", "included"
         ]:
             if attr in column_spec:
                 col = column_spec[attr]

asreview/webapp/utils/project.py
@@ -273,6 +273,7 @@ def get_paper_data(project_id,
                    return_title=True,
                    return_authors=True,
                    return_abstract=True,
+                   return_doi=True,
                    return_debug_label=False):
     """Get the title/authors/abstract for a paper."""
     as_data = read_data(project_id)
@@ -285,14 +286,12 @@ def get_paper_data(project_id,
         paper_data['authors'] = record.authors
     if return_abstract and record.abstract is not None:
         paper_data['abstract'] = record.abstract
+    if return_doi and record.doi is not None:
+        paper_data['doi'] = record.doi
 
     # return the publication data if available
     pub_time = record.extra_fields.get("publish_time", None)
     paper_data['publish_time'] = pub_time if pd.notnull(pub_time) else None
-
-    # return the doi if available
-    doi = record.extra_fields.get("doi", None)
-    paper_data['doi'] = doi if pd.notnull(doi) else None
 
     # return the debug label
     debug_label = record.extra_fields.get("debug_label", None)

asreview/config.py
@@ -49,4 +49,5 @@ COLUMN_DEFINITIONS = {
     "authors": ["authors", "author names", "first_authors"],
     "abstract": ["abstract", "abstract note", "notes_abstract"],
     "keywords": ["keywords"],
+    "doi": ["doi"],
 }

asreview/data/base.py
@@ -322,6 +322,13 @@ class ASReviewData():
         except KeyError:
             return None
 
+    @property
+    def doi(self):
+        try:
+            return self.df[self.column_spec["doi"]].values
+        except KeyError:
+            return None
+
     def get(self, name):
         "Get column with name."
         try:

commit a3e4706dc3e1db2c30ab28b347609e0a354f15ba

Fix bug with DOI introduced in #631 (#654)

asreview/io/paper_record.py
@@ -139,7 +139,11 @@ class PaperRecord():
                 col = attr
             else:
                 col = None
-            setattr(self, attr, kwargs.pop(col, None))
+
+            attr_val = kwargs.pop(col, None)
+            if attr_val is not None and pd.isna(attr_val):
+                attr_val = None
+            setattr(self, attr, attr_val)
 
         self.record_id = record_id
         if self.included is None:

asreview/webapp/utils/project.py
@@ -316,24 +316,16 @@ def clean_all_project_tmp_files():
 
 def get_paper_data(project_id,
                    paper_id,
-                   return_title=True,
-                   return_authors=True,
-                   return_abstract=True,
-                   return_doi=True,
                    return_debug_label=False):
     """Get the title/authors/abstract for a paper."""
     as_data = read_data(project_id)
     record = as_data.record(int(paper_id), by_index=False)
 
     paper_data = {}
-    if return_title and record.title is not None:
-        paper_data['title'] = record.title
-    if return_authors and record.authors is not None:
-        paper_data['authors'] = record.authors
-    if return_abstract and record.abstract is not None:
-        paper_data['abstract'] = record.abstract
-    if return_doi and record.doi is not None:
-        paper_data['doi'] = record.doi
+    paper_data['title'] = record.title
+    paper_data['authors'] = record.authors
+    paper_data['abstract'] = record.abstract
+    paper_data['doi'] = record.doi
 
     # return the debug label
     debug_label = record.extra_fields.get("debug_label", None)

tests/test_readers.py
@@ -54,6 +54,18 @@ def test_nan_values_ris():
     assert as_data.record(0, by_index=True).abstract == ""
     assert as_data.record(2, by_index=True).abstract == ""
 
+    # check missing authors
+    assert as_data.record(0, by_index=True).authors is None
+    assert as_data.record(2, by_index=True).authors is None
+
+    # check missing keywords
+    assert as_data.record(0, by_index=True).keywords is None
+    assert as_data.record(2, by_index=True).keywords is None
+
+    # check missing doi
+    assert as_data.record(0, by_index=True).doi is None
+    assert as_data.record(2, by_index=True).doi is None
+
 
 def test_nan_values_csv():
 
@@ -67,6 +79,18 @@ def test_nan_values_csv():
     # check missing abstracts
     assert as_data.record(0, by_index=True).abstract == ""
     assert as_data.record(2, by_index=True).abstract == ""
+
+    # check missing authors
+    assert as_data.record(0, by_index=True).authors is None
+    assert as_data.record(2, by_index=True).authors is None
+
+    # check missing keywords
+    assert as_data.record(0, by_index=True).keywords is None
+    assert as_data.record(2, by_index=True).keywords is None
+
+    # check missing doi
+    assert as_data.record(0, by_index=True).doi is None
+    assert as_data.record(2, by_index=True).doi is None
 
 
 def test_csv_write_data():