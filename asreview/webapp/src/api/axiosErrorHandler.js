74c07323ae28b9642caa611c3558588d0b06c5b5
Yongchao Ma <terrymyc@gmail.com>
2021-07-27 21:11:18 +0200

    Seperate axios error handler

---
+++ asreview/webapp/src/api/axiosErrorHandler.js
@@ -0,0 +1,24 @@
+const axiosErrorHandler = (error) => {
+  let api_error = {};
+
+  if (error.response) {
+    if (error.response.data["message"]) {
+      api_error["code"] = error.response["status"];
+      api_error["message"] = error.response.data["message"];
+    } else {
+      api_error["code"] = 500;
+      api_error["message"] = "Whoops, something went wrong.";
+    }
+  } else if (error.request) {
+    api_error["code"] = 503;
+    api_error["message"] =
+      "Failed to connect to server. Please restart the software.";
+  } else {
+    api_error["message"] = "Unexpected error.";
+    console.log(error);
+  }
+
+  return api_error;
+};
+
+export default axiosErrorHandler;
