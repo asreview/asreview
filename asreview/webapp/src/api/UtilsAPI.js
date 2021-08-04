bc51719a27c105ea8051137fcd96b3b9b61b80b5
Yongchao Ma <terrymyc@gmail.com>
2021-08-04 10:18:48 +0200

    Adjust utils api

--- asreview/webapp/src/api/UtilsAPI.js
+++ asreview/webapp/src/api/UtilsAPI.js
@@ -4,12 +4,12 @@ import axios from "axios";
 
 class UtilsAPI {
   static faq = () => {
-    const url = "https://raw.githubusercontent.com/asreview/asreview/master/asreview/webapp/faq.json";
+    const url = "https://raw.githubusercontent.com/asreview/asreview/master/asreview/webapp/help.json";
     return new Promise(function (resolve, reject) {
       axios
         .get(url)
         .then((result) => {
-          resolve(result.data["items"]);
+          resolve(result.data.resources.faq["items"]);
         })
         .catch((error) => {
           reject(axiosErrorHandler(error));
