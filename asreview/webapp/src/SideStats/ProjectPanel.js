9413786132b6618eb76218827e2921fb3b02a608
Yongchao Ma <terrymyc@gmail.com>
2021-08-05 09:18:09 +0200

    Italic authors

--- asreview/webapp/src/SideStats/ProjectPanel.js
+++ asreview/webapp/src/SideStats/ProjectPanel.js
@@ -1,27 +1,16 @@
 import React from "react";
 import { ListSubheader, ListItem } from "@material-ui/core";
-// import { makeStyles } from '@material-ui/core/styles';
-
-// const useStyles = makeStyles(theme => ({
-//   root: {
-//     width: '100%',
-//   },
-//   heading: {
-//     fontSize: theme.typography.pxToRem(15),
-//     fontWeight: theme.typography.fontWeightRegular,
-//   },
-// }));
 
 const ProjectPanel = (props) => {
-  // const classes = useStyles();
-
   return (
     <div>
       <ListSubheader component="div" id="list-subheader-project">
         Project
       </ListSubheader>
       <ListItem key="project-name">{props.name}</ListItem>
-      <ListItem key="project-authors">{props.authors}</ListItem>
+      <ListItem key="project-authors">
+        <i>{props.authors ? props.authors : "Unknown authors"}</i>
+      </ListItem>
     </div>
   );
 };
