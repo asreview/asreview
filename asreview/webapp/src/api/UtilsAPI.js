57be0bc135bacadba2623176da6f9e8500ebe55c
Yongchao Terry Ma <terrymyc@gmail.com>
2021-08-09 13:48:38 +0200

    Add Help dialog to the ASReview LAB frontend (#663)

---
+++ asreview/webapp/src/api/UtilsAPI.js
@@ -0,0 +1,21 @@
+import { axiosErrorHandler } from "./index.js";
+import axios from "axios";
+
+class UtilsAPI {
+  static faq = () => {
+    const url =
+      "https://raw.githubusercontent.com/asreview/asreview/master/asreview/webapp/help.json";
+    return new Promise(function (resolve, reject) {
+      axios
+        .get(url)
+        .then((result) => {
+          resolve(result.data.resources.faq["items"]);
+        })
+        .catch((error) => {
+          reject(axiosErrorHandler(error));
+        });
+    });
+  };
+}
+
+export default UtilsAPI;
