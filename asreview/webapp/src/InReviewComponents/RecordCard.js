2a4cff001781a2fe007bb2c2500af744b0fd05ab
Yongchao Ma <terrymyc@gmail.com>
2021-08-09 13:29:37 +0200

    Deepen record card shadow

--- asreview/webapp/src/InReviewComponents/RecordCard.js
+++ asreview/webapp/src/InReviewComponents/RecordCard.js
@@ -15,7 +15,7 @@ const useStyles = makeStyles((theme) => ({
   root: {
     borderRadius: 4,
     boxShadow:
-      "0px 2px 1px -1px rgb(0 0 0 / 20%), 0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 1px 3px 0px rgb(0 0 0 / 12%)",
+      "0px 3px 3px -2px rgb(0 0 0 / 20%), 0px 3px 4px 0px rgb(0 0 0 / 14%), 0px 1px 8px 0px rgb(0 0 0 / 12%)",
     flexDirection: "column",
     height: "-webkit-fill-available",
     margin: "auto",
