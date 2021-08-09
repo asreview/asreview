5c84549d70169f3c6252965fa0e48abc427b64ed
Yongchao Ma <terrymyc@gmail.com>
2021-08-09 09:44:20 +0200

    Remove note sheet in this PR

--- asreview/webapp/src/InReviewComponents/index.js
+++ asreview/webapp/src/InReviewComponents/index.js
@@ -1,7 +1,6 @@
 export { default as DecisionButton } from "./DecisionButton";
 export { default as DecisionUndoBar } from "./DecisionUndoBar";
 export { default as ExplorationModeBanner } from "./ExplorationModeBanner";
-export { default as NoteSheet } from "./NoteSheet";
 export { default as RecordCard } from "./RecordCard";
 export { default as ReviewDialog } from "./ReviewDialog";
 export { default as StatsSheet } from "./StatsSheet";
