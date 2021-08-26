fc6b11fde7fc63bc787803497d091b3bde95bd78
Jonathan de Bruin <jonathandebruinos@gmail.com>
2021-08-26 17:47:23 +0200

    Add file path for state files

--- tests/test_init.py
+++ tests/test_init.py
@@ -8,7 +8,7 @@ from asreview.data import ASReviewData
 data_fp = Path("tests", "demo_data", "generic_labels.csv")
 
 
-def test_init_seed():
+def test_init_seed(tmpdir):
 
     base_start_idx = None
     n_test = 4
@@ -19,7 +19,8 @@ def test_init_seed():
             reviewer = get_reviewer(data_fp,
                                     mode="simulate",
                                     model="nb",
-                                    state_file=None,
+                                    state_file=Path(tmpdir,
+                                                    "tmp_state.asreview"),
                                     init_seed=seed,
                                     n_prior_excluded=1,
                                     n_prior_included=1)
@@ -32,7 +33,7 @@ def test_init_seed():
         assert np.all(np.array(base_start_idx) == np.array(all_start_idx))
 
 
-def test_no_seed():
+def test_no_seed(tmpdir):
     n_test_max = 100
     as_data = ASReviewData.from_file(data_fp)
     n_priored = np.zeros(len(as_data), dtype=int)
@@ -41,7 +42,7 @@ def test_no_seed():
         reviewer = get_reviewer(data_fp,
                                 mode="simulate",
                                 model="nb",
-                                state_file=None,
+                                state_file=Path(tmpdir, "tmp_state.asreview"),
                                 init_seed=None,
                                 n_prior_excluded=1,
                                 n_prior_included=1)
@@ -52,7 +53,7 @@ def test_no_seed():
     raise ValueError(f"Error getting all priors in {n_test_max} iterations.")
 
 
-def test_model_seed():
+def test_model_seed(tmpdir):
     n_test = 4
     seed = 192874123
     last_train_idx = None
@@ -61,7 +62,7 @@ def test_model_seed():
                                 mode="simulate",
                                 model="rf",
                                 query_strategy="random",
-                                state_file=None,
+                                state_file=Path(tmpdir, "tmp_state.asreview"),
                                 init_seed=seed,
                                 seed=seed,
                                 n_prior_excluded=1,
