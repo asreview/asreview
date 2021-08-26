c7d6a5be31cdb1663296f0eaf1d7007727d27486
Jonathan de Bruin <jonathandebruinos@gmail.com>
2021-08-26 14:58:24 +0200

    Deprecate the use of Python 3.6

--- setup.py
+++ setup.py
@@ -94,7 +94,6 @@ setup(
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
@@ -107,7 +106,7 @@ setup(
         'webapp/build/*',
         'webapp/build/static/*/*',
     ]},
-    python_requires='~=3.6',
+    python_requires='~=3.7',
     install_requires=[
         'numpy',
         'sklearn',
