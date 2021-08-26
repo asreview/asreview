14ad1a570e84603d9b320918ec810a937e59e2b4
Jonathan de Bruin <jonathandebruinos@gmail.com>
2021-08-26 16:58:38 +0200

    Remove deprecated API for models

--- asreview/models/__init__.py
+++ asreview/models/__init__.py
@@ -12,9 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# import deprecated models for backwards compatibility
-from asreview.models.deprecated import _moved_warning
-
 from asreview.models.classifiers.nb import NaiveBayesClassifier
 from asreview.models.classifiers.rf import RandomForestClassifier
 from asreview.models.classifiers.svm import SVMClassifier
@@ -26,36 +23,6 @@ from asreview.models.classifiers.utils import get_classifier
 from asreview.models.classifiers.utils import get_classifier_class
 from asreview.models.classifiers.utils import list_classifiers as _list_classifiers
 
-NBModel = _moved_warning(
-    NaiveBayesClassifier, "asreview.models.classifiers.NaiveBayesClassifier",
-    "asreview.models.NBModel")
-RFModel = _moved_warning(
-    RandomForestClassifier, "asreview.models.classifiers.RandomForestClassifier",
-    "asreview.models.RFModel")
-SVMModel = _moved_warning(
-    SVMClassifier, "asreview.models.classifiers.SVMClassifier",
-    "asreview.models.SVMModel")
-LogisticModel = _moved_warning(
-    LogisticClassifier, "asreview.models.classifiers.LogisticClassifier",
-    "asreview.models.LogisticModel")
-LSTMBaseModel = _moved_warning(
-    LSTMBaseClassifier, "asreview.models.classifiers.LSTMBaseClassifier",
-    "asreview.models.LSTMBaseModel")
-LSTMPoolModel = _moved_warning(
-    LSTMPoolClassifier, "asreview.models.classifiers.LSTMPoolClassifier",
-    "asreview.models.LSTMPoolModel")
-NN2LayerModel = _moved_warning(
-    NN2LayerClassifier, "asreview.models.classifiers.NN2LayerClassifier",
-    "asreview.models.NN2LayerModel")
-get_model = _moved_warning(
-    get_classifier, "asreview.models.classifiers.get_classifier",
-    "asreview.models.get_model")
-get_model_class = _moved_warning(
-    get_classifier_class, "asreview.models.classifiers.get_classifier_class",
-    "asreview.models.get_model_class")
-list_classifiers = _moved_warning(
-    _list_classifiers, "asreview.models.classifiers.list_classifiers",
-    "asreview.models.list_classifiers")
 """Active learning model components.
 
 Components like classifiers, query strategies, balance strategies, and
