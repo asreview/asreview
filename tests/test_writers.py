b6abb91fcb81cf88728ef49cd87cf1c16442e810
ottomattas <otto.mattas@gmail.com>
2021-10-16 18:42:33 +0300

    Initialise test for checking notes

--- tests/test_writers.py
+++ tests/test_writers.py
@@ -24,14 +24,26 @@ def test_asreview_labels_ris(test_file,tmpdir):
     # Break for debugging
     #assert False
 
+
+@mark.parametrize("test_file", [
+    ("baseline_tag-notes_labels.ris")
+])
+def test_asreview_notes_ris(test_file,tmpdir):
+    fp_in = Path("tests", "demo_data", test_file)
+    asr_data = ASReviewData.from_file(fp_in)
+    
+    tmp_ris_fp_out = Path("tmp_notes.ris")
+    asr_data.to_ris(tmp_ris_fp_out)
     asr_data_diff = ASReviewData.from_file(tmp_ris_fp_out)
 
     # Check if input file matches the export file
     assert list(asr_data.title) == list(asr_data_diff.title)
-    assert list(asr_data.labels) == list(asr_data_diff.labels)
+    print(asr_data.column_spec)
+    #assert list(asr_data.notes) == list(asr_data_diff.notes)
 
     # Break for debugging
-    # assert False
+    assert False
+
 
 def test_write_numpy_arrays():
     # This test should catch cases where two numpy arrays are to be evaluated in boolean context
