45415868f0acbb649691a8675cb16e4fa5d6375c
Jonathan de Bruin <jonathandebruinos@gmail.com>
2021-11-11 15:27:50 +0100

    Happy lint

--- asreview/state/sql_converter.py
+++ asreview/state/sql_converter.py
@@ -224,8 +224,8 @@ def convert_json_last_probabilities(sql_fp, json_fp):
             for sample_data in
             json_state._state_dict['results'][query]['labelled']
         ]
-        n_records_labeled = len(sf_indices)
-        classifier = json_state.settings.to_dict()['model']
+        # n_records_labeled = len(sf_indices)
+        # classifier = json_state.settings.to_dict()['model']
 
         # Put them in the format for input in the sqlite database.
         last_probabilities = [(proba, ) for proba in last_probabilities]
