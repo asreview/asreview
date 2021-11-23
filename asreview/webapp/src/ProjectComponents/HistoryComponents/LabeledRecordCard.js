8ee3a33246c78d0798a6ff8182b6c87e20f8639f
Yongchao Ma <terrymyc@gmail.com>
2021-11-23 18:17:23 +0100

    Add error handler for mutation classification

--- asreview/webapp/src/ProjectComponents/HistoryComponents/LabeledRecordCard.js
+++ asreview/webapp/src/ProjectComponents/HistoryComponents/LabeledRecordCard.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { useMutation, useQueryClient } from "react-query";
 import TruncateMarkup from "react-truncate-markup";
 import {
+  Box,
   Card,
   CardActions,
   CardContent,
@@ -16,6 +17,7 @@ import { styled } from "@mui/material/styles";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 
+import { InlineErrorHandler } from "../../Components";
 import { ProjectAPI } from "../../api/index.js";
 import { mapStateToProps } from "../../globals.js";
 
@@ -43,102 +45,134 @@ const LabeledRecordCard = (props) => {
   const queryClient = useQueryClient();
   const [recordReadMore, setRecordReadMore] = React.useState(null);
 
-  const { mutate } = useMutation(ProjectAPI.mutateClassification, {
-    onSuccess: (data, variables) => {
-      // update cached data
-      queryClient.setQueryData(
-        [
-          props.label === "relevant"
-            ? "fetchRelevantLabeledRecord"
-            : "fetchIrrelevantLabeledRecord",
-          {
-            project_id: props.project_id,
-            select: props.label === "relevant" ? "included" : "excluded",
-          },
-        ],
-        (prev) => {
-          return {
-            ...prev,
-            pages: prev.pages.map((page) => {
-              return {
-                ...page,
-                result: page.result.map((value) => {
-                  return {
-                    ...value,
-                    included:
-                      value.id === variables.doc_id
-                        ? value.included === 1
-                          ? 0
-                          : 1
-                        : value.included,
-                  };
-                }),
-              };
-            }),
-          };
+  const { error, isError, mutate, reset } = useMutation(
+    ProjectAPI.mutateClassification,
+    {
+      onMutate: () => {
+        if (props.is_prior) {
+          props.setSavingPriorKnowledge(true);
         }
-      );
-    },
-  });
+      },
+      onSuccess: (data, variables) => {
+        if (props.is_prior) {
+          props.setSavingPriorKnowledge(false);
+        }
+        // update cached data
+        queryClient.setQueryData(
+          [
+            props.label === "relevant"
+              ? "fetchRelevantLabeledRecord"
+              : "fetchIrrelevantLabeledRecord",
+            {
+              project_id: props.project_id,
+              select: props.label === "relevant" ? "included" : "excluded",
+            },
+          ],
+          (prev) => {
+            return {
+              ...prev,
+              pages: prev.pages.map((page) => {
+                return {
+                  ...page,
+                  result: page.result.map((value) => {
+                    return {
+                      ...value,
+                      included:
+                        value.id === variables.doc_id
+                          ? value.included === 1
+                            ? 0
+                            : 1
+                          : value.included,
+                    };
+                  }),
+                };
+              }),
+            };
+          }
+        );
+        if (props.is_prior) {
+          queryClient.invalidateQueries("fetchLabeledStats");
+        }
+      },
+    }
+  );
+
+  const resetMutateClassification = () => {
+    reset();
+    if (props.is_prior) {
+      props.setSavingPriorKnowledge(false);
+    }
+  };
 
   return (
     <Root>
-      {props.page.result.map((value) => (
-        <Card elevation={3} className={classes.root} key={value.id}>
-          <CardContent>
-            <Typography gutterBottom variant="h6">
-              {value.title ? value.title : "No title available."}
-            </Typography>
-            <TruncateMarkup
-              lines={value.id === recordReadMore ? Infinity : 6}
-              ellipsis={
-                <span>
-                  ...{" "}
-                  <Link
-                    component="button"
-                    underline="none"
-                    onClick={() => setRecordReadMore(value.id)}
-                  >
-                    read more
-                  </Link>
-                </span>
-              }
-            >
-              <Typography color="textSecondary">
-                {value.abstract ? value.abstract : "No abstract available."}
+      {isError && (
+        <Box sx={{ pt: 8, pb: 16 }}>
+          <InlineErrorHandler
+            message={error["message"]}
+            refetch={resetMutateClassification}
+            button={true}
+          />
+        </Box>
+      )}
+      {!isError &&
+        props.page.result.map((value) => (
+          <Card elevation={3} className={classes.root} key={value.id}>
+            <CardContent>
+              <Typography gutterBottom variant="h6">
+                {value.title ? value.title : "No title available."}
               </Typography>
-            </TruncateMarkup>
-          </CardContent>
-          <CardActions>
-            <Tooltip
-              title={
-                value.included === 1
-                  ? "Convert to irrelevant"
-                  : "Convert to relevant"
-              }
-            >
-              <IconButton
-                className={classes.icon}
-                onClick={() => {
-                  mutate({
-                    project_id: props.project_id,
-                    doc_id: value.id,
-                    label: value.included,
-                    initial: false,
-                  });
-                }}
-                size="large"
+              <TruncateMarkup
+                lines={value.id === recordReadMore ? Infinity : 6}
+                ellipsis={
+                  <span>
+                    ...{" "}
+                    <Link
+                      component="button"
+                      underline="none"
+                      onClick={() => setRecordReadMore(value.id)}
+                    >
+                      read more
+                    </Link>
+                  </span>
+                }
               >
-                {value.included === 1 ? (
-                  <FavoriteIcon color="error" fontSize="small" />
-                ) : (
-                  <FavoriteBorderIcon fontSize="small" />
-                )}
-              </IconButton>
-            </Tooltip>
-          </CardActions>
-        </Card>
-      ))}
+                <Typography color="textSecondary">
+                  {value.abstract ? value.abstract : "No abstract available."}
+                </Typography>
+              </TruncateMarkup>
+            </CardContent>
+            <CardActions>
+              <Tooltip
+                title={
+                  value.included === 1
+                    ? "Convert to irrelevant"
+                    : "Convert to relevant"
+                }
+              >
+                <IconButton
+                  className={classes.icon}
+                  onClick={() => {
+                    mutate({
+                      project_id: props.project_id,
+                      doc_id: value.id,
+                      label: value.included,
+                      initial: false,
+                      is_prior: !props.is_prior ? 0 : 1,
+                    });
+                  }}
+                  size="large"
+                >
+                  {value.included === 1 ? (
+                    <FavoriteIcon color="error" fontSize="small" />
+                  ) : (
+                    <FavoriteBorderIcon fontSize="small" />
+                  )}
+                </IconButton>
+              </Tooltip>
+            </CardActions>
+          </Card>
+        ))}
     </Root>
   );
 };
---
+++ asreview/webapp/src/ProjectComponents/SetupComponents/DataComponents/PriorUnlabeled.js
@@ -0,0 +1,178 @@
+import React from "react";
+import { connect } from "react-redux";
+import { useMutation, useQueryClient } from "react-query";
+import TruncateMarkup from "react-truncate-markup";
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Divider,
+  Link,
+  Typography,
+} from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+import { InlineErrorHandler } from "../../../Components";
+import { ProjectAPI } from "../../../api/index.js";
+import { mapStateToProps } from "../../../globals.js";
+
+const PREFIX = "PriorUnlabeled";
+
+const classes = {
+  root: `${PREFIX}-root`,
+  icon: `${PREFIX}-icon`,
+};
+
+const Root = styled("div")(({ theme }) => ({
+  [`& .${classes.root}`]: {
+    borderRadius: 16,
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(2),
+    maxWidth: 960,
+  },
+
+  [`& .${classes.icon}`]: {
+    marginLeft: "auto",
+  },
+}));
+
+const PriorUnlabeled = (props) => {
+  const queryClient = useQueryClient();
+  const [recordReadMore, setRecordReadMore] = React.useState(null);
+
+  const { error, isError, mutate, reset } = useMutation(
+    ProjectAPI.mutateClassification,
+    {
+      mutationKey: "mutatePriorKnowledge",
+      onMutate: () => {
+        props.setSavingPriorKnowledge(true);
+      },
+      onSuccess: (data, variables) => {
+        props.setSavingPriorKnowledge(false);
+        queryClient.invalidateQueries("fetchLabeledStats");
+        if (!variables.label) {
+          queryClient.invalidateQueries("fetchIrrelevantLabeledRecord");
+        } else {
+          queryClient.invalidateQueries("fetchRelevantLabeledRecord");
+        }
+        if (props.keyword) {
+          // update cached data
+          queryClient.setQueryData(
+            [
+              "fetchPriorSearch",
+              {
+                project_id: props.project_id,
+                keyword: props.keyword,
+              },
+            ],
+            (prev) => {
+              return {
+                ...prev,
+                result: prev.result.map((record) => {
+                  return {
+                    ...record,
+                    included:
+                      record.id === variables.doc_id
+                        ? variables.label
+                        : record.included,
+                  };
+                }),
+              };
+            }
+          );
+        } else {
+          queryClient.invalidateQueries("fetchPriorRandom");
+        }
+      },
+    }
+  );
+
+  const resetMutatePriorKnowledge = () => {
+    reset();
+    props.setSavingPriorKnowledge(false);
+  };
+
+  return (
+    <Root>
+      {isError && (
+        <Box sx={{ pt: 8 }}>
+          <InlineErrorHandler
+            message={error["message"]}
+            refetch={resetMutatePriorKnowledge}
+            button={true}
+          />
+        </Box>
+      )}
+      {!isError && (
+        <Card elevation={3} className={classes.root}>
+          <CardContent>
+            <Typography gutterBottom variant="h6">
+              {props.record.title ? props.record.title : "No title available."}
+            </Typography>
+            <TruncateMarkup
+              lines={props.record.id === recordReadMore ? Infinity : 6}
+              ellipsis={
+                <span>
+                  ...{" "}
+                  <Link
+                    component="button"
+                    underline="none"
+                    onClick={() => setRecordReadMore(props.record.id)}
+                  >
+                    read more
+                  </Link>
+                </span>
+              }
+            >
+              <Typography sx={{ color: "text.secondary" }}>
+                {props.record.abstract
+                  ? props.record.abstract
+                  : "No abstract available."}
+              </Typography>
+            </TruncateMarkup>
+          </CardContent>
+          <Divider />
+          <CardActions sx={{ justifyContent: "space-between" }}>
+            <Typography variant="body2" sx={{ ml: 1 }}>
+              Is this record relevant?
+            </Typography>
+            <Box>
+              <Button
+                onClick={() => {
+                  mutate({
+                    project_id: props.project_id,
+                    doc_id: props.record.id,
+                    label: 1,
+                    initial: true,
+                    is_prior: 1,
+                  });
+                }}
+                size="small"
+              >
+                Yes
+              </Button>
+              <Button
+                onClick={() => {
+                  mutate({
+                    project_id: props.project_id,
+                    doc_id: props.record.id,
+                    label: 0,
+                    initial: true,
+                    is_prior: 1,
+                  });
+                }}
+                size="small"
+              >
+                No
+              </Button>
+            </Box>
+          </CardActions>
+        </Card>
+      )}
+    </Root>
+  );
+};
+
+export default connect(mapStateToProps)(PriorUnlabeled);
