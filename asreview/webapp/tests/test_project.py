3b68393a4a66a9ed156705a549f3e23f28ec0b57
ottomattas <otto.mattas@gmail.com>
2021-11-25 16:29:06 +0200

    Add explanation to RIS status_code 500 check

--- asreview/webapp/tests/test_project.py
+++ asreview/webapp/tests/test_project.py
@@ -122,6 +122,7 @@ def test_project_file(tmp_path, client, url):
     response_export_result_csv = client.get(f"{api_url}/export?file_type=csv")
     response_export_result_tsv = client.get(f"{api_url}/export?file_type=tsv")
     response_export_result_excel = client.get(f"{api_url}/export?file_type=xlsx")
+    # RIS can only be exported from RIS data file path
     assert response_export_result_ris.status_code == 500
     assert response_export_result_csv.status_code == 200
     assert response_export_result_tsv.status_code == 200
