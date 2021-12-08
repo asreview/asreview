9e12807925fabc6109109b0bcf32695f42914cf5
JT <j.j.teijema@gmail.com>
2021-12-08 12:32:53 +0100

    Finish sentence

--- asreview/models/feature_extraction/sbert.py
+++ asreview/models/feature_extraction/sbert.py
@@ -53,6 +53,8 @@ class SBERT(BaseFeatureExtraction):
         This feature extraction technique requires ``sentence_transformers``
         to be installed. Use ``pip install sentence_transformers`` or install
         all optional ASReview dependencies with ``pip install asreview[all]``
+        to install the package.
+
 
     """
 
