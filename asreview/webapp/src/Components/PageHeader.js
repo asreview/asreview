23cc7b94a40d271ba4b675f0bbeb1d20992c7fd8
Yongchao Ma <terrymyc@gmail.com>
2022-01-17 09:01:06 +0100

    Add title to history page

--- asreview/webapp/src/Components/PageHeader.js
+++ asreview/webapp/src/Components/PageHeader.js
@@ -6,7 +6,10 @@ import "../App.css";
 
 export default function PageHeader(props) {
   return (
-    <Box className="main-page-sticky-header-wrapper">
+    <Box
+      className="main-page-sticky-header-wrapper"
+      sx={{ background: (theme) => theme.palette.background.paper }}
+    >
       <Box className="main-page-sticky-header">
         {!props.mobileScreen && (
           <TypographyH5Medium>{props.header}</TypographyH5Medium>
