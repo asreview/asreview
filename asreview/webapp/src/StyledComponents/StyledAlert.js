13b4afeb157d40a4d99cc6ddb2d439cac505ff59
Yongchao Terry Ma <terrymyc@gmail.com>
2022-01-18 09:10:04 +0100

    Add alert to record card of prior selection in exploration mode (#926)

---
+++ asreview/webapp/src/StyledComponents/StyledAlert.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { Alert } from "@mui/material";
+
+export function ExplorationModeRecordAlert(props) {
+  return (
+    <Alert
+      severity="info"
+      sx={{ borderBottomRightRadius: 0, borderBottomLeftRadius: 0 }}
+    >
+      Labeled as relevant in an earlier study
+    </Alert>
+  );
+}
