0dd5254feeaa2d876f111f88a85328c2a5822217
Yongchao Terry Ma <terrymyc@gmail.com>
2022-01-25 09:56:09 +0100

    Fix small UI problems in project setup dialog (#939)

--- asreview/webapp/src/ProjectComponents/index.js
+++ asreview/webapp/src/ProjectComponents/index.js
@@ -1,1 +1,3 @@
+export { default as ProjectInfoForm } from "../ProjectComponents/ProjectInfoForm";
+export { default as ProjectModeSelect } from "../ProjectComponents/ProjectModeSelect";
 export { default as ProjectPage } from "../ProjectComponents/ProjectPage";
