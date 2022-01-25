ad53b9f25cac296de6a31f0cd4d3988969507981
Yongchao Ma <terrymyc@gmail.com>
2022-01-25 15:36:21 +0100

    Prevent typing in filter

--- asreview/webapp/src/ProjectComponents/HistoryComponents/Filter.js
+++ asreview/webapp/src/ProjectComponents/HistoryComponents/Filter.js
@@ -70,6 +70,7 @@ export default function Filter(props) {
               {...rest}
               inputRef={filterInput}
               placeholder={placeholder}
+              readOnly
             />
           );
         }}
