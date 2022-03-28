075b6c0588f666adbabee5598691d088bd96a4fa
Peter Lombaers <peter@idfuse.nl>
2022-03-28 17:37:36 +0200

    Fix converter last_ranking bug
    Fixed a bug in the converter, where it used the record table of the original state file to create the last_ranking table. Now it converts the record ids to the new record table (starting from 0) before it created the last ranking table.

--- asreview/state/sql_converter.py
+++ asreview/state/sql_converter.py
@@ -250,6 +250,11 @@ def create_last_ranking_table(sql_fp, pool_fp, kwargs_fp, json_fp):
                            if record_id not in pool_ranking]
     pool_ranking += records_not_in_pool
 
+    # Convert the records in the pool to the new record ids (starting from 0).
+    old_to_new_record_ids = {old_id: idx
+                             for idx, old_id in enumerate(record_table)}
+    pool_ranking = [old_to_new_record_ids[record] for record in pool_ranking]
+
     # Set the training set to -1 (prior) for records from old pool.
     training_set = -1
     time = None
