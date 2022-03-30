11c4e7b81b09bac31d2ac69a504b91c001d8fc95
Yongchao Ma <terrymyc@gmail.com>
2022-03-30 15:03:38 +0200

    Lint for commit 7d2b5aa

--- asreview/webapp/src/ProjectComponents/ReviewComponents/ReviewPage.js
+++ asreview/webapp/src/ProjectComponents/ReviewComponents/ReviewPage.js
@@ -4,7 +4,7 @@ import { useParams } from "react-router-dom";
 import { Box, Fade } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
-import { ActionsFeedbackBar, PageHeader } from "../../Components";
+import { ActionsFeedbackBar } from "../../Components";
 import {
   DecisionButton,
   DecisionUndoBar,
