485ec9dc1b8055bef4f8964f4e1d114d31dd22ba
Yongchao Terry Ma <terrymyc@gmail.com>
2022-04-21 14:50:35 +0200

    Display wordmark on boot up (#1043)

--- asreview/webapp/src/BootPage.js
+++ asreview/webapp/src/BootPage.js
@@ -1,14 +1,19 @@
 import * as React from "react";
+import ReactLoading from "react-loading";
 import { useQuery } from "react-query";
 import { connect } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { Box, Fade, Stack, Typography } from "@mui/material";
-import { styled } from "@mui/material/styles";
+import { Box, Fade, Stack } from "@mui/material";
+import { styled, useTheme } from "@mui/material/styles";
+
+import { InlineErrorHandler } from "./Components";
 
 import { BaseAPI } from "./api/index.js";
 import { useToggle } from "./hooks/useToggle";
-import { CoronaIcon, ElasIcon, PlusIcon } from "./icons";
 import { setASReviewVersion } from "./redux/actions";
+
+import ASReviewLAB_black from "./images/asreview_sub_logo_lab_black_transparent.svg";
+import ASReviewLAB_white from "./images/asreview_sub_logo_lab_white_transparent.svg";
 
 import "./BootPage.css";
 
@@ -17,14 +22,12 @@ const PREFIX = "BootPage";
 const classes = {
   background: `${PREFIX}-background`,
   root: `${PREFIX}-root`,
-  content: `${PREFIX}-content`,
-  color: `${PREFIX}-color`,
-  error: `${PREFIX}-error`,
+  logo: `${PREFIX}-logo`,
 };
 
 const Root = styled("div")(({ theme }) => ({
   [`& .${classes.background}`]: {
-    backgroundColor: theme.palette.primary.main,
+    backgroundColor: theme.palette.background.paper,
     height: "100%",
     width: "100%",
     position: "absolute",
@@ -36,18 +39,8 @@ const Root = styled("div")(({ theme }) => ({
     justifyContent: "center",
   },
 
-  [`& .${classes.content}`]: {
-    justifyContent: "center",
-  },
-
-  [`& .${classes.color}`]: {
-    color: "#FFF",
-  },
-
-  [`& .${classes.error}`]: {
-    textAlign: "center",
-    marginTop: theme.spacing(3),
-    backgroundColor: "#FFF",
+  [`& .${classes.logo}`]: {
+    width: 600,
   },
 }));
 
@@ -60,6 +53,7 @@ const mapDispatchToProps = (dispatch) => {
 };
 
 const BootPage = ({ setASReviewVersion }) => {
+  const theme = useTheme();
   const navigate = useNavigate();
   const [onAnimation, toggleAnimation] = useToggle(false);
 
@@ -70,100 +64,74 @@ const BootPage = ({ setASReviewVersion }) => {
     [setASReviewVersion]
   );
 
-  const { data, error, isError, isFetching } = useQuery("boot", BaseAPI.boot, {
-    onSuccess: (data) => {
-      // set the version of asreview
-      setVersion(data.version);
-
+  const { error, isError } = useQuery("boot", BaseAPI.boot, {
+    onSettled: (data) => {
       // skip the loader when you are in development mode
-      if (data["status"] === "development") {
+      if (data?.status === "development") {
         navigate("projects");
       } else {
         toggleAnimation();
       }
     },
+    onSuccess: (data) => {
+      // set the version of asreview
+      setVersion(data.version);
+    },
     refetchOnWindowFocus: false,
+    retry: false,
   });
 
-  if (isFetching) {
-    return null;
-  }
+  const wordmarkState = () => {
+    if (theme.palette.mode === "dark") {
+      return ASReviewLAB_white;
+    } else {
+      return ASReviewLAB_black;
+    }
+  };
 
-  if (!isFetching && isError) {
-    return (
-      <Root>
-        <Box className={classes.background}>
-          <Typography className={classes.error} color="error" variant="h4">
-            {error.message !== null
-              ? error.message
-              : "Network Error - Failed to connect to server."}
-          </Typography>
-        </Box>
-      </Root>
-    );
-  }
-
-  /* no errors, continue with bootloading */
-  if (!isFetching && !isError) {
-    return (
-      <Root>
-        <Fade
-          in={onAnimation}
-          timeout={{ enter: 0, exit: 600 }}
-          mountOnEnter
-          unmountOnExit
-          onEnter={() => {
+  return (
+    <Root>
+      <Fade
+        in={onAnimation}
+        timeout={{ enter: 0, exit: 600 }}
+        mountOnEnter
+        unmountOnExit
+        onEnter={() => {
+          if (!isError) {
             setTimeout(() => {
               toggleAnimation();
             }, 3000);
-          }}
-          onExited={() => {
-            navigate("projects");
-          }}
-        >
-          <Box className={classes.background}>
-            <Stack className={classes.root} spacing={6}>
-              <Stack className={classes.content} direction="row">
-                <ElasIcon htmlColor="white" sx={{ fontSize: 160 }} />
-                {data.status === "asreview-covid19" && (
-                  <Box>
-                    <PlusIcon
-                      htmlColor="white"
-                      sx={{ fontSize: 60, mt: "50px", mb: "50px" }}
-                    />
-                    <CoronaIcon
-                      className="rotate"
-                      htmlColor="white"
-                      sx={{ fontSize: 160 }}
-                    />
-                  </Box>
-                )}
-              </Stack>
-              <Stack className={classes.content} direction="row" spacing={3}>
-                <Typography className={classes.color} variant="h3">
-                  ASReview LAB
-                </Typography>
-                {data.status === "asreview-covid19" && (
-                  <Stack direction="row" spacing={3}>
-                    <Typography
-                      className={classes.color}
-                      variant="h5"
-                      sx={{ alignSelf: "center" }}
-                    >
-                      against
-                    </Typography>
-                    <Typography className={classes.color} variant="h3">
-                      COVID-19
-                    </Typography>
-                  </Stack>
-                )}
-              </Stack>
-            </Stack>
-          </Box>
-        </Fade>
-      </Root>
-    );
-  }
+          }
+        }}
+        onExited={() => {
+          navigate("projects");
+        }}
+      >
+        <Box className={classes.background}>
+          <Stack className={classes.root} spacing={3}>
+            <img
+              className={classes.logo}
+              src={wordmarkState()}
+              alt="ASReview LAB"
+            />
+            {!isError && (
+              <ReactLoading
+                type="bubbles"
+                color={
+                  theme.palette.mode === "dark"
+                    ? theme.palette.primary.main
+                    : theme.palette.primary.light
+                }
+                height={100}
+                width={100}
+              />
+            )}
+            {isError && <InlineErrorHandler message={error.message} />}
+          </Stack>
+        </Box>
+      </Fade>
+    </Root>
+  );
 };
 
 export default connect(null, mapDispatchToProps)(BootPage);
