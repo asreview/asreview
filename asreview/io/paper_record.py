da91ea72d1396bb5d353c22900421db0201810db
Yongchao Ma <terrymyc@gmail.com>
2022-05-02 10:33:12 +0200

    Display URL of record when reviewing

--- asreview/io/paper_record.py
+++ asreview/io/paper_record.py
@@ -133,7 +133,7 @@ class PaperRecord():
     def __init__(self, record_id, column_spec={}, **kwargs):
 
         for attr in [
-                "title", "abstract", "authors", "notes", "keywords", "doi", "included"
+                "title", "abstract", "authors", "notes", "keywords", "doi", "url", "included"
         ]:
             if attr in column_spec:
                 col = column_spec[attr]
