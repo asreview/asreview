2d68fdd329269e0865a0c78f348de201e48c9dcb
Jonathan de Bruin <jonathandebruinos@gmail.com>
2022-05-11 21:00:29 +0200

    Move init sampling method to simulate.py because single use (#1081)

--- asreview/review/simulate.py
+++ asreview/review/simulate.py
@@ -16,10 +16,63 @@ from datetime import datetime
 import numpy as np
 import pandas as pd
 
-from asreview.init_sampling import sample_prior_knowledge
 from asreview.project import open_state
 from asreview.review import BaseReview
 from asreview.review.base import LABEL_NA
+from asreview.utils import get_random_state
+
+
+def sample_prior_knowledge(
+        labels, n_prior_included=10,
+        n_prior_excluded=10, random_state=None):
+    """Function to sample prelabelled articles.
+
+    Arguments
+    ---------
+    labels: np.ndarray
+        Array of labels, with 1 -> included, 0 -> excluded.
+    n_prior_included: int
+        The number of positive labels.
+    n_prior_excluded: int
+        The number of negative labels.
+    random_state : int, RandomState instance or None, optional (default=None)
+        If int, random_state is the seed used by the random number generator;
+        If RandomState instance, random_state is the random number generator;
+        If None, the random number generator is the RandomState instance used
+        by `np.random`.
+
+    Returns
+    -------
+    np.ndarray:
+        An array with n_included and n_excluded indices.
+
+    """
+    # set random state
+    r = get_random_state(random_state)
+
+    # retrieve the index of included and excluded papers
+    included_idx = np.where(labels == 1)[0]
+    excluded_idx = np.where(labels == 0)[0]
+
+    if len(included_idx) < n_prior_included:
+        raise ValueError(
+            f"Number of included priors requested ({n_prior_included})"
+            f" is bigger than number of included papers "
+            f"({len(included_idx)}).")
+    if len(excluded_idx) < n_prior_excluded:
+        raise ValueError(
+            f"Number of excluded priors requested ({n_prior_excluded})"
+            f" is bigger than number of excluded papers "
+            f"({len(excluded_idx)}).")
+    # select randomly from included and excluded papers
+    included_indexes_sample = r.choice(
+        included_idx, n_prior_included, replace=False)
+    excluded_indexes_sample = r.choice(
+        excluded_idx, n_prior_excluded, replace=False)
+
+    init = np.append(included_indexes_sample, excluded_indexes_sample)
+
+    return init
 
 
 class ReviewSimulate(BaseReview):
