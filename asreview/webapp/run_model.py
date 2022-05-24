b39e0dbe50941ea14fc34a1a1e28c394ba649534
Jonathan de Bruin <jonathandebruinos@gmail.com>
2022-05-24 17:27:26 +0200

    Fix embedding not found for lstm models (#1116)

--- asreview/webapp/run_model.py
+++ asreview/webapp/run_model.py
@@ -56,9 +56,8 @@ def get_lab_reviewer(as_data,
 
     # LSTM models need embedding matrices.
     if classifier_model.name.startswith("lstm-"):
-        texts = as_data.texts
         classifier_model.embedding_matrix = feature_model.get_embedding_matrix(
-            texts, embedding_fp)
+            as_data.texts, embedding_fp)
 
     # prior knowledge
     if prior_idx is not None and prior_record_id is not None and \
