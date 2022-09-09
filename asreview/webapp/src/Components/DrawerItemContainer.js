7bb39c0faba9bf98332cfebaaa76bb56bfb3fb98
Gijs Mourits <G.Mourits-3@umcutrecht.nl>
2022-09-09 09:19:10 +0200

    Alphabetic ordering
    Add community button and URL

--- asreview/webapp/src/Components/DrawerItemContainer.js
+++ asreview/webapp/src/Components/DrawerItemContainer.js
@@ -19,12 +19,12 @@ import {
 } from "@mui/material";
 
 import { styled } from "@mui/material/styles";
-import { Help, Payment, Settings, Diversity3 } from "@mui/icons-material";
+import { Diversity3, Help, Payment, Settings } from "@mui/icons-material";
 
 import { DrawerItem, ElasGame } from "../Components";
 
 import { ProjectAPI } from "../api/index.js";
-import { donateURL, communityURL, projectModes, projectStatuses } from "../globals.js";
+import { communityURL, donateURL, projectModes, projectStatuses } from "../globals.js";
 import Finished from "../images/ElasHoldingSIGNS_Finished.svg";
 import InReview from "../images/ElasHoldingSIGNS_InReview.svg";
 import SetUp from "../images/ElasHoldingSIGNS_SetUp.svg";
