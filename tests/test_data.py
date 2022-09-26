45d224d1e5a6637e255453cd8fe761f660a15ee9
Gijs Mourits <gijsmourits1998@hotmail.com>
2022-09-26 13:43:16 +0200

    improve example

--- tests/test_data.py
+++ tests/test_data.py
@@ -6,6 +6,7 @@ from pytest import mark
 
 import asreview
 from asreview.data.statistics import n_duplicates
+from asreview.data.base import ASReviewData
 from asreview.datasets import DatasetManager
 from asreview.search import fuzzy_find
 
@@ -53,12 +54,42 @@ def test_datasets(data_name):
 
 
 def test_data_statistics():
-
     d = asreview.ASReviewData(
         pd.DataFrame({
-            "title": ["a", "b", "b", "c"],
-            "abstract": ["lorem", "lorem", "lorem", "lorem"]
+            "title": ["a", "a", "b", "c", "d", "e", "f", "g", "h", "i"],
+            "abstract": ["lorem", "lorem", "lorem", "lorem", "lorem", "lorem", "lorem", "lorem", "lorem", "lorem"],
+            "doi": ["10.1", "", "10.3", "10.3", "", "", "   ", "   ", None, None]
         })
     )
 
-    assert n_duplicates(d) == 1
+    assert n_duplicates(d) == 2
+
+
+def test_data_base():
+    d_dups = ASReviewData(
+        pd.DataFrame({
+            "title": ["a", "a", "b", "c", "d", "e", "f", "g", "h", "i"],
+            "abstract": ["lorem", "lorem", "lorem", "lorem", "lorem", "lorem", "lorem", "lorem", "lorem", "lorem"],
+            "doi": ["10.1", "", "10.3", "10.3", "", "", "   ", "   ", None, None]
+        })
+    )
+
+    d_nodups = ASReviewData(
+        pd.DataFrame({
+            "title": ["a", "b", "d", "e", "f", "g", "h", "i"],
+            "abstract": ["lorem", "lorem", "lorem", "lorem", "lorem", "lorem", "lorem", "lorem"],
+            "doi": ["10.1", "10.3", "", "", "   ", "   ", None, None]
+        })
+    )
+
+    d_removed_dups = ASReviewData(
+        pd.DataFrame({
+            "title": ["a", "c"],
+            "abstract": ["lorem", "lorem"],
+            "doi": ["10.2", "10.3"]
+        })
+    )
+
+    pd.testing.assert_frame_equal(d_dups.drop_duplicates(), d_nodups)
+
+    pd.testing.assert_frame_equal(d_dups.duplicated(), d_removed_dups)
