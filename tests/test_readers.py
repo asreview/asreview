746db5c2b7652985c8b3d21188a7fc2345daf950
Gijs Mourits <gijsmourits1998@hotmail.com>
2022-10-19 10:49:20 +0200

    Add support for RIS reading from URL in ASReviewData (#1246)

--- tests/test_readers.py
+++ tests/test_readers.py
@@ -4,6 +4,7 @@ from pytest import mark
 import rispy
 
 from asreview import ASReviewData
+from asreview.utils import is_url
 
 
 @mark.parametrize("test_file,n_lines,ignore_col",
@@ -16,9 +17,16 @@ from asreview import ASReviewData
                    ("generic_tab.tsv", 2, []), ("generic_labels.csv", 6, []),
                    ("pubmed_zotero.ris", 6, []), ("pubmed_endnote.txt", 6, []),
                    ("scopus.ris", 6, []), ("ovid_zotero.ris", 6, []),
-                   ("proquest.ris", 6, [])])
+                   ("proquest.ris", 6, []),
+                   ("https://raw.githubusercontent.com/asreview/systematic-"
+                    "review-datasets/master/datasets/van_de_Schoot_2017/raw/"
+                    "schoot-lgmm-ptsd-included-3.ris", 8, [])])
 def test_reader(test_file, n_lines, ignore_col):
-    fp = Path("tests", "demo_data", test_file)
+    if is_url(test_file):
+        fp = test_file
+    else:
+        fp = Path("tests", "demo_data", test_file)
+
     as_data = ASReviewData.from_file(fp)
     assert len(as_data) == n_lines
 
