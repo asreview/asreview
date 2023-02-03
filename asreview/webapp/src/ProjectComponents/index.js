824795a48956d0663222f8b3789103a7326d9eb9
Yongchao Ma <terrymyc@gmail.com>
2023-02-02 20:48:05 -0500

    Remove mode select in project setup

--- asreview/webapp/src/ProjectComponents/index.js
+++ asreview/webapp/src/ProjectComponents/index.js
@@ -2,6 +2,5 @@ export { default as ImportFromFile } from "./ImportFromFile";
 export { default as ImportProject } from "./ImportProject";
 export { default as ProjectDeleteDialog } from "./ProjectDeleteDialog";
 export { default as ProjectInfoForm } from "./ProjectInfoForm";
-export { default as ProjectModeSelect } from "./ProjectModeSelect";
 export { default as ProjectPage } from "./ProjectPage";
 export { default as SelectItem } from "./SelectItem";
