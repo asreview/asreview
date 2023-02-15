7330cc99064976186fc88506b4d79d8db1112044
Jonathan de Bruin <jonathandebruinos@gmail.com>
2023-02-15 17:53:16 +0100

    Revert changes to untouched submodules

--- asreview/io/excel_writer.py
+++ asreview/io/excel_writer.py
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-class ExcelWriter:
-    """Excel file writer."""
+class ExcelWriter():
+    """Excel file writer.
+    """
 
     name = "xlsx"
     label = "Excel"
