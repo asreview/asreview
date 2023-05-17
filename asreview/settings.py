be2a4e21787ebf81250b911287b5ef895f4655aa
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2023-05-17 11:52:24 +0200

    fixed line length error

--- asreview/settings.py
+++ asreview/settings.py
@@ -12,7 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Constructing the settings object that holds the model choices and their parameterization."""
+"""
+Constructing the settings object that holds the model choices
+and their parameterization.
+"""
 
 import logging
 import os
