9efa2691bb290cb1839487fdc87203b1702d8718
Yongchao Ma <terrymyc@gmail.com>
2023-06-02 13:27:27 +0200

    Revert limit on user name

--- asreview/webapp/authentication/models.py
+++ asreview/webapp/authentication/models.py
@@ -82,8 +82,8 @@ class User(UserMixin, DB.Model):
     def validate_name(self, _key, name):
         if not bool(name):
             raise ValueError("Name is required")
-        elif len(name) < 1:
-            raise ValueError("Name must be at least 1 character long")
+        elif len(name) < 3:
+            raise ValueError("Name must contain more than 2 characters")
         return name
 
     @validates("email", "hashed_password")
