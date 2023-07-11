5a60bfc6d55e2a527e00840156b2956a542f147b
voismager <gerafish2398@yandex.ru>
2023-07-11 13:40:59 +0200

    UI prototype

--- asreview/webapp/src/ProjectComponents/ReviewComponents/LabelsTable.js
+++ asreview/webapp/src/ProjectComponents/ReviewComponents/LabelsTable.js
@@ -1,73 +1,98 @@
 import React from "react";
-import { Box, Fab, Stack } from "@mui/material";
-import { styled } from "@mui/material/styles";
-import { Favorite, FavoriteBorder } from "@mui/icons-material";
+import {Box, Typography, Checkbox, FormGroup, FormControlLabel, Card, CardContent} from "@mui/material";
+import {styled} from "@mui/material/styles";
 import "./ReviewPage.css";
 
 const PREFIX = "LabelsTable";
 
 const classes = {
-    extendedFab: `${PREFIX}-extendedFab`,
+    categoryCard: `${PREFIX}-categoryCard`,
+    title: `${PREFIX}-title`,
 };
 
-const Root = styled("div")(({ theme }) => ({
-    [`& .${classes.extendedFab}`]: {
-        marginRight: theme.spacing(1),
+const categories = [
+    {
+        name: "Biomes",
+        labels: [
+            "Boreal Forest",
+            "Savanna",
+            "Mangrove",
+            "Tropical Forest",
+            "Forest"
+        ]
+    },
+    {
+        name: "Restoration Approaches",
+        labels: [
+            "Direct seeding (i.e. spreading/planting seeds)",
+            "Planting trees (i.e. planting trees as seedlings)",
+            "Enrichment planting (i.e. planting trees under existing forest)",
+            "Direct seeding (i.e. spreading/planting seeds)",
+            "Assisted natural regeneration",
+            "Farmer managed natural regeneration"
+        ]
+    },
+    {
+        name: "Recovery Data",
+        labels: [
+            "Measured carbon (or biomass)",
+            "Diversity of plant species naturally recruiting",
+            "Changes in vegetation structure (i.e. basal area, canopy cover)",
+            "Recovery of animal/insect diversity",
+            "Social benefits"
+        ]
+    }
+]
+
+const Root = styled("div")(({theme}) => ({
+    display: "flex",
+    flex: "1 0 auto",
+    margin: "auto",
+    maxWidth: 960,
+    padding: "108px 0px 32px 0px",
+    height: "100%",
+
+    [`& .${classes.categoryCard}`]: {
+        borderRadius: 16,
+        marginBottom: "16px",
+        display: "flex",
+        flexDirection: "column",
+        width: "100%"
+    },
+
+    [`& .${classes.title}`]: {
+        lineHeight: 1.2,
     },
 }));
 
 const LabelsTable = (props) => {
-    let relevantLabel = "Relevant";
-    let irrelevantLabel = "Irrelevant";
-
-    if (props.previousRecord.show) {
-        if (props.previousRecord.label === 0) {
-            relevantLabel = "Convert to relevant";
-            irrelevantLabel = "Keep irrelevant";
-        }
-        if (props.previousRecord.label === 1) {
-            relevantLabel = "Keep relevant";
-            irrelevantLabel = "Convert to irrelevant";
-        }
-    }
-
     return (
         <Root>
-            <Stack
-                className="review-page-decision-button"
-                direction={
-                    !props.mobileScreen
-                        ? "row"
-                        : !props.previousRecord.show
-                            ? "row"
-                            : "column"
-                }
-                spacing={!props.mobileScreen ? 10 : !props.previousRecord.show ? 10 : 2}
-            >
-                <Box>
-                    <Fab
-                        disabled={props.disableButton()}
-                        onClick={() => props.makeDecision(0)}
-                        size={props.mobileScreen ? "small" : "large"}
-                        variant="extended"
-                    >
-                        <FavoriteBorder className={classes.extendedFab} />
-                        {irrelevantLabel}
-                    </Fab>
-                </Box>
-                <Box>
-                    <Fab
-                        onClick={() => props.makeDecision(1)}
-                        color="primary"
-                        disabled={props.disableButton()}
-                        size={props.mobileScreen ? "small" : "large"}
-                        variant="extended"
-                    >
-                        <Favorite className={classes.extendedFab} />
-                        {relevantLabel}
-                    </Fab>
-                </Box>
-            </Stack>
+            <Box>
+                {/*<Typography*/}
+                {/*    component="div"*/}
+                {/*    className={classes.title}*/}
+                {/*    variant={!props.mobileScreen ? "h5" : "h6"}*/}
+                {/*    sx={{*/}
+                {/*        fontWeight: (theme) => theme.typography.fontWeightRegular,*/}
+                {/*    }}*/}
+                {/*>*/}
+                {/*    Select categories*/}
+                {/*</Typography>*/}
+
+                {categories.map((category) => (
+                    <Card elevation={2} className={classes.categoryCard}>
+                        <CardContent>
+                            <Typography variant="h6">{category.name}</Typography>
+                            <FormGroup row="false">
+                                {category.labels.map((categoryLabel) => (
+                                    <FormControlLabel control={<Checkbox/>} label={categoryLabel}/>
+                                ))}
+                            </FormGroup>
+                        </CardContent>
+                    </Card>
+                ))}
+            </Box>
         </Root>
     );
 };
