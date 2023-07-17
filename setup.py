9d77a378f3ac724af4481ec35b82557cb2ac6c6b
Jonathan de Bruin <jonathandebruinos@gmail.com>
2023-07-17 15:16:12 +0200

    Restrict tensorflow versions to prevent Keras error

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def get_long_description():
 DEPS = {
     "sbert": ['sentence_transformers'],
     "doc2vec": ['gensim'],
-    "tensorflow": ['tensorflow~=2.0'],
+    "tensorflow": ['tensorflow>=2.0,<2.13.0'],
     "dev": ['check-manifest'],
     'test': ['coverage', 'pytest'],
 }
