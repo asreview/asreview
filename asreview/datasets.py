58a49b2ad6d43497cd4c789e7d40564eea3675cc
Jonathan de Bruin <jonathandebruinos@gmail.com>
2023-11-02 07:53:41 +0100

    Fix SYNERGY visible in Oracle mode setup (#1556)

--- asreview/datasets.py
+++ asreview/datasets.py
@@ -15,6 +15,7 @@
 import json
 import socket
 import tempfile
+import warnings
 from abc import ABC
 from abc import abstractmethod
 from pathlib import Path
@@ -737,7 +738,6 @@ class SynergyDataGroup(BaseDataGroup):
 
 class BenchmarkDataGroup(BaseDataGroup):
     """Datasets available in the benchmark platform.
-
     Deprecated
     """
 
@@ -745,6 +745,14 @@ class BenchmarkDataGroup(BaseDataGroup):
     description = "DEPRECATED: Datasets available in the online benchmark platform"
 
     def __init__(self):
+
+        warnings.warn(
+            "The use of 'benchmark' datasets is deprecated, "
+            "use SYNERGY dataset instead. For more information, see "
+            "https://github.com/asreview/synergy-dataset.",
+            category=UserWarning
+        )
+
         meta_file = "https://raw.githubusercontent.com/asreview/systematic-review-datasets/master/index_v1.json"  # noqa
         datasets = _download_from_metadata(meta_file)
 
