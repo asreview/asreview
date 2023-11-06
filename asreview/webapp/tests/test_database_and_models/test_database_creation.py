6cd7355ba213a101bceb87dc930ad0b1fb75bf90
Jonathan de Bruin <jonathandebruinos@gmail.com>
2023-11-06 15:18:45 +0100

    Refactor development workflow for flask app (#1568)

--- asreview/webapp/tests/test_database_and_models/test_database_creation.py
+++ asreview/webapp/tests/test_database_and_models/test_database_creation.py
@@ -1,6 +1,3 @@
-# GOAL: test database creation if app is started with authentication
-from pathlib import Path
-
 import pytest
 import sqlalchemy
 from sqlalchemy import create_engine
@@ -9,20 +6,20 @@ from asreview.utils import asreview_path
 
 
 def get_db_path():
-    return Path(asreview_path() / "asreview.test.sqlite")
+    return asreview_path() / "asreview.test.sqlite"
 
 
 # checks if asreview path does not contain a database if app
 # is unauthenticated
 def test_database_is_not_created_if_unauth_app(unauth_app):
-    assert Path(asreview_path()).exists()
+    assert asreview_path().exists()
     assert get_db_path().exists() is False
 
 
 # checks is asreview path contains database if app is
 # authenticated
 def test_database_exists_after_starting_auth_app(auth_app):
-    assert Path(asreview_path()).exists()
+    assert asreview_path().exists()
     assert get_db_path().exists()
 
 
@@ -31,6 +28,6 @@ def test_database_exists_after_starting_auth_app(auth_app):
     "table", ["collaboration_invitations", "collaborations", "projects", "users"]
 )
 def test_if_db_table_exists(auth_app, table):
-    engine = create_engine(f"sqlite:///{str(get_db_path())}")
+    engine = create_engine(f"sqlite:///{get_db_path()}")
     table_names = sqlalchemy.inspect(engine).get_table_names()
     assert table in table_names
