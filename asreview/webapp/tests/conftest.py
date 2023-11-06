d1e9b6b5a3fd42307220d15e99f4b3a985e60acb
Jonathan de Bruin <jonathandebruinos@gmail.com>
2023-11-06 15:18:45 +0100

    Refactor development workflow for flask app (#1568)

--- asreview/webapp/tests/conftest.py
+++ asreview/webapp/tests/conftest.py
@@ -19,7 +19,7 @@ import pytest
 from sqlalchemy.orm import close_all_sessions
 
 from asreview.webapp import DB
-from asreview.webapp.start_flask import create_app
+from asreview.webapp.app import create_app
 from asreview.webapp.tests.utils import crud
 
 PROJECTS = [
@@ -55,7 +55,7 @@ def _get_app(app_type="auth-basic", path=None):
     else:
         raise ValueError(f"Unknown config {app_type}")
     # create app
-    app = create_app(flask_configfile=config_path)
+    app = create_app(env="test", config_file=config_path)
     # and return it
     return app
 
