908f484439499e28bb02fb3d3ce431efa2d738d8
Peter Lombaers <peter@idfuse.nl>
2023-11-16 09:28:19 +0100

    Complete everything outside of webapp

--- asreview/search.py
+++ asreview/search.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+__all__ = ["SearchError", "fuzzy_find"]
+
 import re
 from difflib import SequenceMatcher
 
--- asreview/utils.py
+++ asreview/utils.py
@@ -12,6 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+__all__ = [
+    "asreview_path",
+    "get_data_home",
+    "get_random_state",
+]
+
 import functools
 import os
 import sys
