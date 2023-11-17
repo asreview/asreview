6403b7ebe90281dab0c0fcc45e4522cb298b29c5
Rens van de schoot <a.g.j.vandeschoot@uu.nl>
2023-11-17 16:47:42 +0100

    Add button to copy citation

--- asreview/webapp/src/Components/CiteDialog.js
+++ asreview/webapp/src/Components/CiteDialog.js
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
 import {
+  Box,
   Dialog,
   DialogTitle,
   DialogContent,
   Divider,
+  IconButton,
+  List,
   ListItem,
-  Typography,
+  ListItemText,
+  Snackbar,
   useTheme,
 } from "@mui/material";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 import { StyledTextButton } from "../StyledComponents/StyledButton.js";
 import { styled } from "@mui/material/styles";
 
@@ -53,22 +58,41 @@ const mapStateToProps = (state) => {
 
 const CiteDialog = ({ isOpen, onClose, mobileScreen, asreview_version }) => {
   const theme = useTheme();
-  const [selectedStyle, setSelectedStyle] = useState("APA");
-  const handleStyleClick = (style) => setSelectedStyle(style);
+  const copyButtonSize = theme.spacing(6);
+  const [selectedStyle, setSelectedStyle] = useState("RIS");
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+
+  const copyToClipboard = (citationText) => {
+    navigator.clipboard.writeText(citationText).then(() => {
+      setSnackbarOpen(true);
+    });
+  };
+
+  const handleCloseSnackbar = () => {
+    setSnackbarOpen(false);
+  };
+
+  const handleStyleClick = (style) => {
+    setSelectedStyle(style);
+  };
 
   const citationStyles = {
+    RIS: [
+      `TY  - COMP`,
+      `AU  - ASReview LAB developers`,
+      `PY  - 2023`,
+      `TI  - ASReview LAB: A Tool for AI-Assisted Systematic Reviews [Software v.1.4]`,
+      `PB  - Zenodo`,
+      `UR  - https://doi.org/10.5281/zenodo.3345592`,
+      `DO  - 10.5281/ZENODO.10084260`,
+      `ER  - `,
+    ].join("\n"),
     APA: [
       `ASReview LAB developers. (2023). ASReview LAB: A tool for AI-assisted systematic reviews [Software v.${asreview_version}]. Zenodo. https://doi.org/10.5281/zenodo.3345592`,
-    ],
+    ].join("\n"),
     MLA: [
       `ASReview LAB developers. "ASReview LAB: A Tool for AI-Assisted Systematic Reviews [Software v.${asreview_version}]." Zenodo, 2023, https://doi.org/10.5281/zenodo.3345592.`,
-    ],
-    Chicago: [
-      `ASReview LAB developers. 2023. "ASReview LAB: A Tool for AI-Assisted Systematic Reviews [Software v.${asreview_version}]." Zenodo. https://doi.org/10.5281/zenodo.3345592.`,
-    ],
-    Vancouver: [
-      `ASReview LAB developers. ASReview LAB: A Tool for AI-Assisted Systematic Reviews [Software v.${asreview_version}]. Zenodo; 2023. Available from: https://doi.org/10.5281/zenodo.3345592`,
-    ],
+    ].join("\n"),
     BibTex: [
       `@software{asreviewlab2023,`,
       `title={ASReview LAB: A Tool for AI-Assisted Systematic Reviews [Software v.${asreview_version}]},`,
@@ -77,17 +101,34 @@ const CiteDialog = ({ isOpen, onClose, mobileScreen, asreview_version }) => {
       `url={https://doi.org/10.5281/zenodo.3345592},`,
       `note={Software version: ${asreview_version}}`,
       `}`,
-    ],
-    RIS: [
-      `TY  - COMP`,
-      `AU  - ASReview LAB developers`,
-      `PY  - 2023`,
-      `TI  - ASReview LAB: A Tool for AI-Assisted Systematic Reviews [Software v.${asreview_version}]`,
-      `PB  - Zenodo`,
-      `UR  - https://doi.org/10.5281/zenodo.3345592`,
-      `DO  - 10.5281/ZENODO.10084260`,
-      `ER  - `,
-    ],
+    ].join("\n"),
+    Chicago: [
+      `ASReview LAB developers. 2023. "ASReview LAB: A Tool for AI-Assisted Systematic Reviews [Software v.${asreview_version}]." Zenodo. https://doi.org/10.5281/zenodo.3345592.`,
+    ].join("\n"),
+    Vancouver: [
+      `ASReview LAB developers. ASReview LAB: A Tool for AI-Assisted Systematic Reviews [Software v.${asreview_version}]. Zenodo; 2023. Available from: https://doi.org/10.5281/zenodo.3345592`,
+    ].join("\n"),
+  };
+
+  const renderCitationText = (style) => {
+    return citationStyles[style].split("\n").map((line, index) => (
+      <ListItem key={index} sx={{ padding: 0, display: "block" }}>
+        <ListItemText
+          primary={line}
+          primaryTypographyProps={{
+            variant: "caption",
+            sx: {
+              padding: 0,
+              display: "block",
+              whiteSpace: "pre-line",
+
+              textOverflow: "ellipsis",
+              paddingRight: copyButtonSize,
+            },
+          }}
+        />
+      </ListItem>
+    ));
   };
 
   return (
@@ -117,32 +158,41 @@ const CiteDialog = ({ isOpen, onClose, mobileScreen, asreview_version }) => {
           orientation={mobileScreen ? "horizontal" : "vertical"}
           flexItem
         />
-        <div
-          style={{
+
+        <Box
+          sx={{
             flex: 1,
-            overflow: "auto",
-            padding: theme.spacing(2),
+            position: "relative",
+            overflow: "hidden",
+            padding: theme.spacing(1),
             backgroundColor: theme.palette.grey[200],
             borderRadius: theme.shape.borderRadius,
           }}
         >
-          {citationStyles[selectedStyle].map((line, index) => (
-            <div
-              key={index}
-              style={{
-                margin: mobileScreen ? theme.spacing(1, 0) : theme.spacing(0),
-              }}
-            >
-              <Typography
-                variant={mobileScreen ? "body2" : "subtitle1"}
-                sx={{ color: theme.palette.text.secondary }}
-              >
-                {line}
-              </Typography>
-            </div>
-          ))}
-        </div>
+          <List sx={{ overflow: "auto" }}>
+            {renderCitationText(selectedStyle)}
+          </List>
+
+          <IconButton
+            size="small"
+            onClick={() => copyToClipboard(citationStyles[selectedStyle])}
+            sx={{
+              position: "absolute",
+              right: theme.spacing(1),
+              top: theme.spacing(1),
+            }}
+          >
+            <ContentCopyIcon fontSize="small" />
+          </IconButton>
+        </Box>
       </StyledDialogContent>
+
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={3000}
+        onClose={handleCloseSnackbar}
+        message="Citation copied to clipboard"
+      />
     </Dialog>
   );
 };
