b051e058fab5717c176ab1f03ab45d233da3d54b
cskaandorp <casper@compunist.nl>
2023-11-21 22:13:00 +0100

    README enhancement, added required selenium library. Version problems are fixed

--- setup.py
+++ setup.py
@@ -81,7 +81,12 @@ DEPS = {
     "doc2vec": ["gensim"],
     "tensorflow": ["tensorflow~=2.0"],
     "dev": ["black", "check-manifest", "flake8", "flake8-isort", "isort"],
-    "test": ["coverage", "pytest", "pytest-random-order"],
+    "test": [
+        "coverage",
+        "pytest",
+        "pytest-random-order",
+        "pytest-selenium"
+    ],
     "docs": [
         "ipython",
         "sphinx",
