0276be87b9e99068dfe856588828b509dbf7526d
Lisa Gaudette <lisa.gaudette@gmail.com>
2023-11-20 20:32:06 -0500

    Don't allow empty categories
    Add tags at the same time as creating categories, with additional
    validation.

--- asreview/webapp/src/ProjectComponents/TagComponents/TagEditor.js
+++ asreview/webapp/src/ProjectComponents/TagComponents/TagEditor.js
@@ -15,7 +15,8 @@ import {
   DialogTitle,
 } from "@mui/material";
 
-import { ExpandMore } from "@mui/icons-material";
+
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 import { TypographySubtitle1Medium } from "../../StyledComponents/StyledTypography.js";
 
@@ -55,13 +56,22 @@ const Tag = (props) => {
   );
 };
 
-const AddDialog = (props) => {
+function nameToId(name) {
+  // Generate a suggested ID based on name
+  // since Ids may be used later in data analysis code we suggest simple ascii 
+  // with no spaces but this is not required
+  return name.toLowerCase().replaceAll(/\s+/g, "_").replaceAll(/[^a-z0-9_]/g, "")
+}
+
+const AddTagDialog = (props) => {
   const [name, setName] = React.useState("");
   const [id, setId] = React.useState("");
+  const [idEdited, setIdEdited] = React.useState(false)
 
   const reset = () => {
     setName("");
     setId("");
+    setIdEdited(false)
   };
 
   const addClicked = () => {
@@ -75,6 +85,19 @@ const AddDialog = (props) => {
     reset();
   };
 
+  const editName = (newName) => {
+    setName(newName);
+    if(!idEdited) {
+      setId(nameToId(newName))
+    }
+  }
+
+  const editId = (newId) => {
+    setId(newId);
+    setIdEdited(true)
+  }
+
+  const duplicatedId = props.tags.some((t) => t.id === id)
   return (
     <Dialog open={props.open} onClose={handleClose}>
       <DialogTitle>{props.title}</DialogTitle>
@@ -88,22 +111,165 @@ const AddDialog = (props) => {
               id="tag-name"
               label="Name"
               value={name}
-              onChange={(event) => setName(event.target.value)}
+              helperText=" "
+              onChange={(event) => editName(event.target.value)}
             />
             <TextField
               fullWidth
               id="tag-id"
               label="Id"
+              helperText={duplicatedId ? "Tag Ids must be unique within the category" : " "}
               value={id}
-              onChange={(event) => setId(event.target.value)}
+              onChange={(event) => editId(event.target.value)}
             />
           </Stack>
         </Stack>
       </DialogContent>
       <DialogActions>
         <Button onClick={handleClose}>Cancel</Button>
-        <Button onClick={addClicked} disabled={name === "" || id === ""}>
+        <Button onClick={addClicked} disabled={name === "" || id === "" || duplicatedId}>
           Add
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+function idsUnique(items) {
+  const idList = items.map((t) => t.id)
+  const idSet = new Set(idList)
+  return idSet.size === idList.length
+}
+
+const AddCategoryDialog = (props) => {
+  const emptyTag = {id: "", name: "", idEdited: false}
+
+  const [name, setName] = React.useState("");
+  const [id, setId] = React.useState("");
+  const [idEdited, setIdEdited] = React.useState(false)
+  const [tags, setTags] = React.useState([emptyTag]);
+
+  const reset = () => {
+    setName("");
+    setId("");
+    setIdEdited(false)
+    setTags([emptyTag]);
+  };
+
+  const addClicked = () => {
+    let values = tags.filter((t) => t.id !== "" && t.name !== "")
+    values = values.map((t) => {return {id: t.id, name: t.name}});
+    props.handleAdd(id, name, values);      
+    props.handleClose();
+    reset();
+  };
+
+  const handleClose = () => {
+    props.handleClose();
+    reset();
+  };
+
+  const editTag = (newName, newId, index) => {
+    setTags(tags.map((t, i) => {
+      if(i === index) {
+        // Editing if
+        if(t.id !== newId) {
+          //
+          t.id = newId
+          t.idEdited = true
+        }
+        if(t.name !== newName) {
+          t.name = newName
+          if (!t.idEdited) {
+            t.id = nameToId(newName)
+          }
+        }
+       }
+       return t;
+    }));
+  }
+
+  const addTag = () => {
+    setTags([...tags, {id: "", name: "", idEdited: false}])
+  }
+
+  const duplicatedCategoryId = props.categories.some((c) => c.id === id)
+
+
+  const tagsUnique = idsUnique(tags)
+
+  const tagsValid = tags.filter((t) => t.id !== "" && t.name !== "").length > 0
+
+  return (
+    <Dialog open={props.open} onClose={handleClose}>
+      <DialogTitle>{props.title}</DialogTitle>
+      <DialogContent>
+        <DialogContentText>{props.contentText}</DialogContentText>
+        <Stack spacing={3}>
+          <TypographySubtitle1Medium>Category Details</TypographySubtitle1Medium>
+          <Stack direction="row" spacing={3}>
+            <TextField
+              fullWidth
+              id="category-name"
+              label="Name"
+              value={name}
+              onChange={(event) => {
+                  setName(event.target.value); 
+                  if(!idEdited) {
+                    setId(nameToId(event.target.value))
+                  }
+                }}
+              helperText=" "
+            />
+            <TextField
+              fullWidth
+              id="category-id"
+              label="Id"
+              value={id}
+              onChange={(event) => {
+                setId(event.target.value)
+                setIdEdited(true)
+              }}
+              helperText={duplicatedCategoryId ? "Category Ids must be unique" : " "}
+            />
+          </Stack>
+        </Stack>
+        <Stack spacing={3}>
+          <TypographySubtitle1Medium>Tags</TypographySubtitle1Medium>
+          {tags.map((t, index) => 
+            <Stack direction="row" spacing={3} key={index}>
+            <TextField
+              fullWidth
+              id="tag-name"
+              label="Name"
+              value={t.name}
+              onChange={(event) => editTag(event.target.value, t.id, index)}
+            />
+            <TextField
+              fullWidth
+              id="tag-id"
+              label="Id"
+              value={t.id}
+              onChange={(event) => editTag(t.name, event.target.value, index)}
+            />
+          </Stack>
+        )}
+          
+        </Stack>
+        <Stack
+          direction="row"
+          justifyContent="flex-end"
+          alignItems="baseline"
+          spacing={2}>
+          <Button aria-label="add tag" onClick={addTag}>Add Tag</Button>
+        </Stack>
+        
+        <Typography variant="body2" sx={{ color: "text.secondary" }}>Ids must be unique and can't be changed after creating the category.</Typography>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Cancel</Button>
+        <Button onClick={addClicked} disabled={name === "" || id === "" || !tagsValid  || !tagsUnique}>
+          Create Category
         </Button>
       </DialogActions>
     </Dialog>
@@ -136,7 +302,7 @@ const Category = (props) => {
 
   return (
     <Accordion elevation={3}>
-      <AccordionSummary expandIcon={<ExpandMore />}>
+      <AccordionSummary expandIcon={<ExpandMoreIcon />}>
         <Stack
           direction={!props.mobileScreen ? "row" : "column"}
           sx={{ width: "100%" }}
@@ -152,12 +318,13 @@ const Category = (props) => {
         </Stack>
       </AccordionSummary>
       <AccordionDetails>
-        <AddDialog
+        <AddTagDialog
           title="Create Tag"
           text="Create a tag. The id can't be changed after creation."
           open={newTagDialogOpen}
           handleClose={() => setNewTagDialogOpen(false)}
           handleAdd={addTag}
+          tags={props.category.values}
         />
 
         <Stack spacing={3}>
@@ -231,13 +398,13 @@ const TagEditor = (props) => {
       ]);
     }
   };
-  const addTagCategory = (id, name) => {
-    props.handleTagChange([...tags, { name: name, values: [], id: id }]);
+  const addTagCategory = (id, name, values) => {
+    props.handleTagChange([...tags, { name: name, values: values, id: id }]);
   };
 
   return (
     <Accordion elevation={3}>
-      <AccordionSummary expandIcon={<ExpandMore />}>
+      <AccordionSummary expandIcon={<ExpandMoreIcon />}>
         <Typography
           sx={{ width: !props.mobileScreen ? "33%" : "100%", flexShrink: 0 }}
         >
@@ -247,12 +414,14 @@ const TagEditor = (props) => {
       </AccordionSummary>
       <AccordionDetails>
         <TypographySubtitle1Medium>Categories</TypographySubtitle1Medium>
-        <AddDialog
+        <AddCategoryDialog
           title="Add Category"
-          text="Create a tag category. The id can't be changed after creation."
+          text="Create a tag category. The ids can't be changed after creation."
           open={categoryDialogOpen}
           handleClose={() => setCategoryDialogOpen(false)}
           handleAdd={addTagCategory}
+          handleAddTags={editTagCategory}
+          categories={props.tags}
         />
         {tags.map((c) => (
           <Category
