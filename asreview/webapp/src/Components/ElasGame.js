44eece72803e04162bac446b8990ad02130b3a07
Rens van de schoot <a.g.j.vandeschoot@uu.nl>
2023-11-22 20:57:18 +0100

    Improve logic, add scoring per mode, add reset button

--- asreview/webapp/src/Components/ElasGame.js
+++ asreview/webapp/src/Components/ElasGame.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState, useCallback } from "react";
 import {
+  Button,
   Grid,
   Paper,
   Stack,
+  Typography,
   Box,
   Switch,
   FormControlLabel,
@@ -80,82 +82,172 @@ const GameStyle = styled(Box)(({ theme }) => ({
   },
 }));
 
+function shuffleImages(images) {
+  let shuffledImages = [...images];
+  for (let i = shuffledImages.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledImages[i], shuffledImages[j]] = [
+      shuffledImages[j],
+      shuffledImages[i],
+    ];
+  }
+  return shuffledImages;
+}
+
+const initialShuffle = (mode) => {
+  const imagesToUse = mode === "simple" ? images.slice(0, 8) : images;
+  const duplicatedImages = imagesToUse.concat(imagesToUse);
+  return shuffleImages(duplicatedImages);
+};
+
 const ElasGame = (props) => {
   const [mode, setMode] = useState("simple");
   const [cheatMode, setCheatMode] = useState(false);
-  const [imagesArray, setImagesArray] = useState([]);
   const [paperSelected, setPaperSelected] = useState([]);
   const [paperSelectedIds, setPaperSelectedIds] = useState([]);
-  const [openCards, setOpenCards] = useState([]);
+  const [openCards] = useState([]);
   const [showingCheatCards, setShowingCheatCards] = useState(false);
-  const [shuffledOnce, setShuffledOnce] = useState(false);
+  const [gameStarted, setGameStarted] = useState(false);
+  const [simpleModeOpenCards, setSimpleModeOpenCards] = useState([]);
+  const [expertModeOpenCards, setExpertModeOpenCards] = useState([]);
+  const [simpleModeAttempts, setSimpleModeAttempts] = useState(0);
+  const [expertModeAttempts, setExpertModeAttempts] = useState(0);
+  const [simpleModeMatches, setSimpleModeMatches] = useState(0);
+  const [expertModeMatches, setExpertModeMatches] = useState(0);
+  const [canToggleCheatMode, setCanToggleCheatMode] = useState(true);
+  const [imagesArray, setImagesArray] = useState([]);
+
+  const [shuffledSimpleImages, setShuffledSimpleImages] = useState(() =>
+    initialShuffle("simple"),
+  );
+  const [shuffledExpertImages, setShuffledExpertImages] = useState(() =>
+    initialShuffle("expert"),
+  );
 
   const toggleMode = () => {
-    setMode((prevMode) => (prevMode === "simple" ? "expert" : "simple"));
-    setShuffledOnce(false);
+    setMode((prevMode) => {
+      if (prevMode === "simple") {
+        setImagesArray(shuffledExpertImages);
+        return "expert";
+      } else {
+        setImagesArray(shuffledSimpleImages);
+        return "simple";
+      }
+    });
   };
+
+  const resetGame = () => {
+    setSimpleModeOpenCards([]);
+    setExpertModeOpenCards([]);
+    setGameStarted(false);
+
+    const shuffledSimple = initialShuffle("simple");
+    const shuffledExpert = initialShuffle("expert");
+
+    setShuffledSimpleImages(shuffledSimple);
+    setShuffledExpertImages(shuffledExpert);
+
+    if (mode === "simple") {
+      setImagesArray(shuffledSimple);
+    } else {
+      setImagesArray(shuffledExpert);
+    }
+  };
+
+  function updateScoreAndAttempts(isMatch) {
+    if (mode === "simple") {
+      setSimpleModeAttempts((prev) => prev + 1);
+      if (isMatch) {
+        setSimpleModeMatches((prev) => prev + 1);
+      }
+    } else {
+      setExpertModeAttempts((prev) => prev + 1);
+      if (isMatch) {
+        setExpertModeMatches((prev) => prev + 1);
+      }
+    }
+  }
 
   const handleKeyPress = useCallback(
     (event) => {
-      if (event.key === "c" && cheatMode === false) {
+      if (event.key === "c" && !cheatMode && canToggleCheatMode) {
         setCheatMode(true);
-        setShuffledOnce(true);
+        setCanToggleCheatMode(false);
       }
     },
-    [cheatMode],
+    [cheatMode, canToggleCheatMode],
   );
 
   function flipImage(image, index) {
-    if (paperSelectedIds?.length === 1 && paperSelectedIds[0] === index) {
+    const currentModeOpenCards =
+      mode === "simple" ? simpleModeOpenCards : expertModeOpenCards;
+    const setCurrentModeOpenCards =
+      mode === "simple" ? setSimpleModeOpenCards : setExpertModeOpenCards;
+
+    // Check if the clicked card is already matched (open)
+    if (currentModeOpenCards.includes(image)) {
       return;
     }
 
-    if (paperSelected?.length < 2) {
-      setPaperSelected((paperSelected) => paperSelected?.concat(image));
-      setPaperSelectedIds((paperSelectedIds) =>
-        paperSelectedIds?.concat(index),
-      );
+    // Check if the same card is clicked again
+    if (paperSelectedIds.includes(index)) {
+      return;
+    }
 
-      if (paperSelected?.length === 1) {
+    // Check if less than two cards are selected
+    if (paperSelected.length < 2) {
+      setPaperSelected((prevSelected) => [...prevSelected, image]);
+      setPaperSelectedIds((prevIds) => [...prevIds, index]);
+
+      // If two cards are selected, check for a match
+      if (paperSelected.length === 1) {
         if (paperSelected[0] === image) {
-          setOpenCards((openCards) =>
-            openCards?.concat([paperSelected[0], image]),
-          );
-        }
-        setTimeout(() => {
-          setPaperSelectedIds([]);
+
+          // Cards match
+          setCurrentModeOpenCards((prevOpenCards) => [
+            ...prevOpenCards,
+            paperSelected[0],
+            image,
+          ]);
           setPaperSelected([]);
-        }, 700);
-        props.addAttempt();
+          setPaperSelectedIds([]);
+          updateScoreAndAttempts(true);
+        } else {
+          setTimeout(() => {
+            setPaperSelected([]);
+            setPaperSelectedIds([]);
+            updateScoreAndAttempts(false);
+          }, 700);
+        }
       }
     }
   }
 
   function isCardChosen(image, index) {
-    return paperSelectedIds?.includes(index) || openCards?.includes(image);
-  }
-
-  function shuffle(arr) {
-    for (let i = arr.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [arr[i], arr[j]] = [arr[j], arr[i]];
-    }
-    return arr;
+    const currentOpenCards =
+      mode === "simple" ? simpleModeOpenCards : expertModeOpenCards;
+    return currentOpenCards.includes(image);
   }
 
   useEffect(() => {
-    const imagesToUse = mode === "simple" ? images.slice(0, 8) : images;
-    const imagesGenerated = imagesToUse.concat(...imagesToUse);
-    if (!shuffledOnce) {
-      const shuffledImages = shuffle(imagesGenerated);
-      setImagesArray(shuffledImages);
+    setImagesArray(
+      mode === "simple" ? shuffledSimpleImages : shuffledExpertImages,
+    );
+  }, [mode, shuffledSimpleImages, shuffledExpertImages]);
+
+  useEffect(() => {
+    if (!gameStarted) {
+      setGameStarted(true);
     }
+  }, [gameStarted]);
+
+  useEffect(() => {
     window.addEventListener("keydown", handleKeyPress);
 
     return () => {
       window.removeEventListener("keydown", handleKeyPress);
     };
-  }, [handleKeyPress, mode, shuffledOnce]);
+  }, [handleKeyPress]);
 
   useEffect(() => {
     if (cheatMode) {
@@ -163,7 +255,8 @@ const ElasGame = (props) => {
 
       const hideCheatCardsTimeout = setTimeout(() => {
         setShowingCheatCards(false);
-        setCheatMode(false); // Reset cheat mode after revealing one pair
+        setCheatMode(false);
+        setCanToggleCheatMode(true);
       }, 1000);
 
       return () => {
@@ -174,22 +267,52 @@ const ElasGame = (props) => {
 
   return (
     <GameStyle>
-      <FormControlLabel
-        control={
-          <Switch
-            checked={mode === "expert"}
-            onChange={toggleMode}
-            name="mode"
-            color="primary"
-          />
-        }
-        label="Expert Mode"
-      />
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          padding: 2,
+        }}
+      >
+        <FormControlLabel
+          control={
+            <Switch
+              checked={mode === "expert"}
+              onChange={toggleMode}
+              name="mode"
+              color="primary"
+              disabled={paperSelected.length === 1}
+            />
+          }
+          label="Expert Mode"
+        />
+
+        <Typography variant="h6">
+          {mode === "simple"
+            ? `Simple Mode: ${simpleModeMatches} correct out of ${simpleModeAttempts}`
+            : `Expert Mode: ${expertModeMatches} correct out of ${expertModeAttempts}`}
+        </Typography>
+
+        <Button
+          variant="outlined"
+          size="small"
+          onClick={resetGame}
+          style={{
+            minWidth: "30px",
+            height: "30px",
+            padding: "0 8px",
+            marginRight: "10px",
+          }}
+        >
+          Reset
+        </Button>
+      </Box>
+
       <Grid container justifyContent="center" spacing={2}>
         {imagesArray?.map((image, index) => (
           <Grid key={index} item>
             <Paper
-              key={index}
               onClick={() => flipImage(image, index)}
               className={classes.paper}
               component={Stack}
@@ -197,6 +320,7 @@ const ElasGame = (props) => {
               justifyContent="center"
             >
               {isCardChosen(image, index) ||
+              paperSelectedIds.includes(index) ||
               (showingCheatCards && !openCards.includes(image)) ? (
                 <img src={image} alt="" className={classes.image} />
               ) : (
