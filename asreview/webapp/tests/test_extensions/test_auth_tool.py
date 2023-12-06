09b958bfaeb78c1d418b7f4ba51f44e8622e696d
Jonathan de Bruin <jonathandebruinos@gmail.com>
2023-12-06 21:05:55 +0100

    Fix auth-tool import in test module

--- asreview/webapp/tests/test_extensions/test_auth_tool.py
+++ asreview/webapp/tests/test_extensions/test_auth_tool.py
@@ -6,9 +6,9 @@ from uuid import uuid4
 
 import pytest
 
-import asreview.entry_points.auth_tool as tool
+import asreview.webapp.entry_points.auth_tool as tool
 from asreview import ASReviewProject
-from asreview.entry_points.auth_tool import AuthTool
+from asreview.webapp.entry_points.auth_tool import AuthTool
 from asreview.state.sql_converter import upgrade_asreview_project_file
 from asreview.utils import asreview_path
 from asreview.webapp import DB
