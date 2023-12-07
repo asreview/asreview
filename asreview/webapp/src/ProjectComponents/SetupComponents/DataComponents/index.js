f6a71094568f336f4013174678d7d60169b3a09d
Jonathan de Bruin <jonathandebruinos@gmail.com>
2023-12-07 10:55:50 +0100

    Revert DatasetImport index

--- asreview/webapp/src/ProjectComponents/SetupComponents/DataComponents/index.js
+++ asreview/webapp/src/ProjectComponents/SetupComponents/DataComponents/index.js
@@ -6,6 +6,7 @@ export { default as DatasetFromFile } from "./DatasetFromFile";
 export { default as DatasetFromURL } from "./DatasetFromURL";
 export { default as EnoughPriorBanner } from "./EnoughPriorBanner";
 export { default as EntryPointDataset } from "./EntryPointDataset";
+export { default as ImportDataset } from "./ImportDataset";
 export { default as PriorLabeled } from "./PriorLabeled";
 export { default as PriorRandom } from "./PriorRandom";
 export { default as PriorSearch } from "./PriorSearch";
