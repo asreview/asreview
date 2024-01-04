0c269f9539c261ca4f1ee245a442748fbd48c2d2
Jonathan de Bruin <jonathandebruinos@gmail.com>
2024-01-04 22:48:42 +0100

    Remove StyledAlertPrior

--- asreview/webapp/src/StyledComponents/StyledAlert.js
+++ asreview/webapp/src/StyledComponents/StyledAlert.js
@@ -17,7 +17,7 @@ export function ExplorationModeRecordAlert(props) {
         >
           {props.label}
         </Box>
-      }, what would be your decision?
+      }{props.prior ? "" : ", what would be your decision?"}
     </Alert>
   );
 }
