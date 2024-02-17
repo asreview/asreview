b3943957c25c4e83fc0c66a29cd49dd66b7574b6
Jonathan de Bruin <jonathandebruinos@gmail.com>
2024-02-17 14:52:31 +0100

    Improve styling of Stepper

--- asreview/webapp/src/ProjectComponents/SetupComponents/SetupStepper.js
+++ asreview/webapp/src/ProjectComponents/SetupComponents/SetupStepper.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Stepper, Step, StepButton, StepLabel } from "@mui/material";
-import { styled } from "@mui/material/styles";
 
 const steps = [
   "Project information",
@@ -9,64 +8,43 @@ const steps = [
   "Screen options",
 ];
 
-const PREFIX = "SetupStepper";
-
-const classes = {
-  step: `${PREFIX}-step`,
-};
-
-const Root = styled("div")(({ theme }) => ({
-  paddingTop: "16px",
-  [`& .${classes.step}`]: {
-    padding: "0 80px",
-  },
-}));
-
 const SetupStepper = ({ activeStep, handleStep, completed, isStepFailed }) => {
   return (
-    <Root>
-      <Stepper alternativeLabel activeStep={activeStep}>
-        {steps.map((label, index) => {
-          const isError = isStepFailed(index);
-          return (
-            <Step
-              className={classes.step}
-              key={label}
-              completed={index < activeStep}
+    <Stepper
+      alternativeLabel
+      activeStep={activeStep}
+      sx={{ padding: "12px 6px 24px 6px" }}
+    >
+      {steps.map((label, index) => {
+        const isError = isStepFailed(index);
+        return (
+          <Step key={label} completed={index < activeStep}>
+            <StepButton
+              color="inherit"
+              onClick={handleStep(index)}
+              disabled={false}
+              sx={(theme) => ({
+                borderRadius: "4px",
+                "&:hover": {
+                  backgroundColor:
+                    theme.palette.mode === "dark"
+                      ? "rgba(255, 205, 0, 0.12)"
+                      : "rgba(129, 103, 0, 0.12)",
+                },
+              })}
             >
-              <StepButton
-                color="inherit"
-                onClick={handleStep(index)}
-                disabled={false}
-                sx={(theme) => ({
-                  borderRadius: "4px",
-                  "&:hover": {
-                    backgroundColor:
-                      theme.palette.mode === "dark"
-                        ? "rgba(255, 205, 0, 0.12)"
-                        : "rgba(129, 103, 0, 0.12)",
-                  },
-                })}
+              <StepLabel
+                StepIconProps={{
+                  sx: { width: isError ? "22px" : "19.5px", height: "22px" },
+                }}
               >
-                <StepLabel
-                  // StepIconComponent={
-                  //   isError || (index < activeStep && activeStep !== index)
-                  //     ? StepIcon
-                  //     : StyledStepIcon
-                  // }
-                  StepIconProps={{
-                    sx: { width: isError ? "22px" : "19.5px", height: "22px" },
-                  }}
-                  // {...(isError ? { error: true } : {})}
-                >
-                  {label}
-                </StepLabel>
-              </StepButton>
-            </Step>
-          );
-        })}
-      </Stepper>
-    </Root>
+                {label}
+              </StepLabel>
+            </StepButton>
+          </Step>
+        );
+      })}
+    </Stepper>
   );
 };
 
