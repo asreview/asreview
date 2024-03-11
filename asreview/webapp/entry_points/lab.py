11370702f68315d065da573d62f376792b5f8e48
Jonathan de Bruin <jonathandebruinos@gmail.com>
2024-03-11 12:01:30 +0100

    Update asreview/webapp/entry_points/lab.py

--- asreview/webapp/entry_points/lab.py
+++ asreview/webapp/entry_points/lab.py
@@ -168,7 +168,7 @@ def lab_entry_point(argv):
     # deprecated, remove in version 2.0
     if args.certfile or args.keyfile:
         print(
-            "SSL/TLS is deprecated for the built-in server and will be removed. "
+            "SSL/TLS is deprecated for the built-in server and will be removed in version 2. "
             "Use a dedicated WSGI server (e.g. gUnicorn) instead to make use of TLS. "
             "See https://flask.palletsprojects.com/en/3.0.x/deploying/\n\n"
         )
