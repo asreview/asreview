fd9ed6169326f017c8348078e0a944e49abdf453
Laurens de Bruin <laurens.de.bruin99@gmail.com>
2024-04-11 10:37:23 +0200

    Add color grades to charts

--- asreview/webapp/src/ProjectComponents/AnalyticsComponents/DatasetChart.js
+++ asreview/webapp/src/ProjectComponents/AnalyticsComponents/DatasetChart.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Chart from "react-apexcharts";
-import useTheme from "@mui/material/styles";
+import { useTheme } from "@mui/material/styles";
 
 export default function DatasetChart({ label, part, total }) {
   const theme = useTheme();
@@ -9,106 +9,86 @@ export default function DatasetChart({ label, part, total }) {
     return Math.round((part / total) * 1000) / 10 + "%";
   }, [total, part]);
 
-  /**
-   * Chart data array
-   */
-  const seriesArray = React.useCallback(() => {
-    if (part && total) {
-      return [Math.round((part / total) * 10000) / 100];
+  const chartcolor = () => {
+    if (part / total < 0.75) {
+      return ["#F44336"];
+    } else if (part / total < 0.95) {
+      return ["#FFC107"];
     } else {
-      return [];
+      return ["#4CAF50"];
     }
-  }, [part, total]);
-
-  /**
-   * Chart options
-   */
-  const optionsChart = React.useCallback(() => {
-    return {
-      chart: {
-        animations: {
-          enabled: false,
-        },
-        background: "transparent",
-        id: "ASReviewLABprogressChart",
-        type: "radialBar",
-      },
-      plotOptions: {
-        radialBar: {
-          hollow: {
-            margin: 15,
-            size: "60%",
-          },
-          dataLabels: {
-            name: {
-              fontSize: "22px",
-            },
-            value: {
-              fontSize: theme.typography.h5.fontSize,
-              fontFamily: theme.typography.h5.fontFamily,
-              fontWeight: theme.typography.fontWeightBold,
-            },
-            total: {
-              show: true,
-              label: label,
-              fontSize: theme.typography.subtitle1.fontSize,
-              fontFamily: theme.typography.subtitle1.fontFamily,
-              color: theme.palette.text.secondary,
-              formatter: formattedTotal,
-            },
-          },
-        },
-      },
-      colors: [
-        theme.palette.mode === "light"
-          ? theme.palette.secondary.light
-          : theme.palette.secondary.main,
-        theme.palette.mode === "light"
-          ? theme.palette.primary.light
-          : theme.palette.primary.main,
-      ],
-      dataLabels: {
-        enabled: false,
-      },
-      fill: {
-        type: "gradient",
-        gradient: {
-          shade: "light",
-          type: "horizontal",
-          shadeIntensity: 0,
-          inverseColors: true,
-          opacityFrom: 0.7,
-          opacityTo: 0.9,
-          stops: [0, 100],
-        },
-      },
-      markers: {
-        size: 0,
-      },
-      noData: {
-        text: "No data available",
-      },
-      stroke: {
-        lineCap: "round",
-      },
-      theme: {
-        mode: theme.palette.mode,
-      },
-    };
-  }, [theme, formattedTotal]);
-
-  const [series, setSeries] = React.useState(seriesArray());
-  const [options, setOptions] = React.useState({});
-
-  React.useEffect(() => {
-    setSeries(seriesArray());
-    setOptions(optionsChart());
-  }, [seriesArray, optionsChart]);
+  };
 
   return (
     <Chart
-      options={options}
-      series={series}
+      options={{
+        chart: {
+          animations: {
+            enabled: false,
+          },
+          background: "transparent",
+          id: "ASReviewLABDatasetChart",
+          type: "radialBar",
+        },
+        plotOptions: {
+          radialBar: {
+            hollow: {
+              margin: 15,
+              size: "60%",
+            },
+            dataLabels: {
+              name: {
+                fontSize: "22px",
+              },
+              value: {
+                fontSize: theme.typography.h5.fontSize,
+                fontFamily: theme.typography.h5.fontFamily,
+                fontWeight: theme.typography.fontWeightBold,
+              },
+              total: {
+                show: true,
+                label: label,
+                fontSize: theme.typography.subtitle1.fontSize,
+                fontFamily: theme.typography.subtitle1.fontFamily,
+                color: theme.palette.text.secondary,
+                formatter: formattedTotal,
+              },
+            },
+          },
+        },
+        colors: [
+          theme.palette.mode === "light"
+            ? theme.palette.secondary.light
+            : theme.palette.secondary.main,
+          theme.palette.mode === "light"
+            ? theme.palette.primary.light
+            : theme.palette.primary.main,
+        ],
+        dataLabels: {
+          enabled: false,
+        },
+        fill: {
+          colors: chartcolor(),
+          gradient: {
+            shade: "light",
+            type: "horizontal",
+            shadeIntensity: 0,
+            opacityFrom: 0.7,
+            opacityTo: 0.9,
+            stops: [0, 100],
+          },
+        },
+        markers: {
+          size: 0,
+        },
+        noData: {
+          text: "No data available",
+        },
+        theme: {
+          mode: theme.palette.mode,
+        },
+      }}
+      series={[Math.round((part / total) * 10000) / 100]}
       type="radialBar"
       height={350}
       width="100%"
