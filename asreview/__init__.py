a53c290ed30ca4d68abdf531fa82449785fc7ec9
Jonathan de Bruin <jonathandebruinos@gmail.com>
2024-05-15 20:54:10 +0200

    Fix docs import

--- asreview/__init__.py
+++ asreview/__init__.py
@@ -21,7 +21,7 @@ from asreview.project import Project
 from asreview.state.contextmanager import open_state
 from asreview.utils import asreview_path
 from asreview.utils import get_data_home
-from asreview.simulation import Simulate
+from asreview.simulation.simulate import Simulate
 
 try:
     from asreview._version import __version__
