d082ede2bc71b132725f5d66cda8e1f3593b69fe
Jonathan de Bruin <jonathandebruinos@gmail.com>
2024-05-22 23:13:59 +0200

    Remove seed utils

--- asreview/utils.py
+++ asreview/utils.py
@@ -12,9 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__all__ = [
-    "get_random_state",
-]
+__all__ = []
 
 from pathlib import Path
 from urllib.error import HTTPError
@@ -64,63 +62,3 @@ def _is_url(url):
         )
     except Exception:
         return False
-
-
-def get_random_state(seed=None):
-    """Constructor for the seeded random number generator.
-
-    This is the preferred method of instantiating the SeededRandomState class.
-    This function makes sure that the random number generator has been seeded properly.
-    By having a seperate constructor function, we are following the same pattern as
-    Numpy uses.
-
-    Parameters
-    ----------
-    seed : int | SeededRandomState | None, optional
-        Seed for the random generator, or random generator itself. If this is None, a
-        seed is generated randomly. By default None.
-
-    Returns
-    -------
-    SeededRandomState
-        A random number generator, seeded by the provided seed.
-    """
-    if isinstance(seed, SeededRandomState):
-        return seed
-    # # For the newer np.random.Generator class, the seed setting would be as follows:
-    # # https://numpy.org/doc/stable/reference/random/index.html#quick-start
-    # if seed is None:
-    #     seed = secrets.randbits(128)
-    if seed is None:
-        rng = np.random.default_rng()
-        seed = int(rng.integers(low=0, high=2**32))
-    if not isinstance(seed, int):
-        raise ValueError(
-            "'seed' should be of type int, SeededRandomNumberGenerator or None"
-        )
-    return SeededRandomState(np.random.RandomState(seed), seed)
-
-
-class SeededRandomState(np.random.RandomState):
-    def __init__(self, random_state, seed):
-        """Random State that is always seeded.
-
-        A wrapper class around np.random.Generator that has the attribute `seed` added.
-        Never instantiate this class directly, always use the `get_random_generator`
-        function. It is implicitly assumed that the generator has been seeded using the
-        seed from the argument. The function `get_random_generator` makes sure this
-        happens correctly.
-
-        Parameters
-        ----------
-        random_state : np.random.RandomState
-            Random number generator that has been instantiated using
-            `np.random.RandomState(seed)`.
-        seed : int
-            Integer that has been used as the seed of the generator.
-        """
-        # For the newer `np.random.Generator` class the init argument would be
-        # `generator.bit_generator`. For the legacy `np.random.RandomState` class you
-        # you can get the seed by `random_state.get_state()[1][0]`.
-        super().__init__(random_state.get_state()[1][0])
-        self.seed = seed
