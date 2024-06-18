55e5066d1ce930369c23b99635b798ac67cb0fa4
Caserias <b.yazan@students.uu.nl>
2024-06-18 17:44:09 +0200

    Update tickAmount in ProgressDensityChart and ProgressRecallChart to make the intervals consistent and accurate.

--- asreview/webapp/src/ProjectComponents/AnalyticsComponents/ProgressRecallChart.js
+++ asreview/webapp/src/ProjectComponents/AnalyticsComponents/ProgressRecallChart.js
@@ -188,6 +188,10 @@ export default function ProgressRecallChart(props) {
    * Chart options
    */
   const optionsChart = React.useCallback(() => {
+    const maxYValue = maxY() || 0;
+    const tickAmount = 7;
+    const closestDivisibleBy7 = Math.ceil(maxYValue / tickAmount) * tickAmount; // To make the intervals consistent, max value in the y-axis should be always divisible by 7.
+
     return {
       chart: {
         animations: {
@@ -265,9 +269,10 @@ export default function ProgressRecallChart(props) {
           },
         },
         showAlways: false,
-        max: maxY(),
+        max: closestDivisibleBy7,
         forceNiceScale: false,
-        tickAmount: maxY() < 6 ? maxY() : 6,
+        min: 0,
+        tickAmount: tickAmount,
         title: {
           text: "Number of relevant records",
         },
