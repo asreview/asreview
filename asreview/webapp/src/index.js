ce2e28c6bb6a50ba6f191fafa12c033268affc8b
Jonathan de Bruin <jonathandebruinos@gmail.com>
2024-09-04 20:39:05 +0200

    Update and remove various NPM dependencies  (#1831)

--- asreview/webapp/src/index.js
+++ asreview/webapp/src/index.js
@@ -1,22 +1,27 @@
 import React from "react";
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import { AuthProvider } from "context/AuthProvider";
 import store from "redux/store";
 import App from "App";
 
-render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <AuthProvider>
-          <Routes>
-            <Route path="*" element={<App />} />
-          </Routes>
-        </AuthProvider>
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("root"),
-);
+function ASReviewApp() {
+  return (
+    <React.StrictMode>
+      <Provider store={store}>
+        <BrowserRouter>
+          <AuthProvider>
+            <Routes>
+              <Route path="*" element={<App />} />
+            </Routes>
+          </AuthProvider>
+        </BrowserRouter>
+      </Provider>
+    </React.StrictMode>
+  );
+}
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<ASReviewApp />);
