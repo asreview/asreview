df490f6ea7e2beefb509627ab5163777f22e85c9
Jonathan de Bruin <jonathandebruinos@gmail.com>
2024-09-30 18:04:19 +0200

    Apply colors from PR #1859

---
+++ asreview/webapp/src/constants/theme.js
@@ -0,0 +1,54 @@
+export const colorScheme = {
+  light: {
+    palette: {
+      primary: {
+        main: "#bf9906",
+      },
+      secondary: {
+        main: "#A5C18E",
+      },
+      background: {
+        default: "#FFFFFA",
+        paper: "#F7F7F2",
+      },
+      text: {
+        primary: "#3E2723",
+        secondary: "#6D4C41",
+      },
+      error: { main: "#D32F2F" },
+
+      grey: {
+        400: "#DEDBD2",
+        600: "#4A5759",
+        800: "#37474F",
+      },
+    },
+  },
+  dark: {
+    palette: {
+      primary: {
+        main: "#FFCC00",
+      },
+      secondary: {
+        main: "#64B5F6",
+      },
+      background: {
+        default: "#121212",
+        paper: "#1E1E1E",
+      },
+      text: {
+        primary: "#FFFFFF",
+        secondary: "#B0B0B0",
+      },
+      error: {
+        main: "#FF6B6B",
+      },
+
+      grey: {
+        400: "#4d4d4d",
+        600: "#6F7E6F",
+        800: "#B0B0B0",
+      },
+    },
+  },
+};
