b11a84a1b6d66e4bd7094fd8b64996e63c7de820
Casper Kaandorp <casper@compunist.nl>
2024-10-09 09:59:51 +0200

    logging failures in task manager

--- asreview/webapp/queue/task_manager.py
+++ asreview/webapp/queue/task_manager.py
@@ -151,6 +151,9 @@ class TaskManager:
                 self.insert_in_waiting(project_id, simulation)
 
             elif action in ["remove", "failure"] and project_id:
+                if action == "failure":
+                    logging.error(
+                        f"Failed to train model for project {project_id}")
                 self.remove_pending(project_id)
 
     def _handle_incoming_messages(self, conn):
@@ -165,7 +168,8 @@ class TaskManager:
                     if client_buffer != "":
                         # we may be dealing with multiple messages,
                         # update buffer to produce a correct json string
-                        client_buffer = "[" + client_buffer.replace("}{", "},{") + "]"
+                        client_buffer = \
+                            "[" + client_buffer.replace("}{", "},{") + "]"
                         messages = json.loads(client_buffer)
                         logging.info(f"Received messages:\n{messages}\n")
                         # add to buffer
