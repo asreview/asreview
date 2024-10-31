fc07fbfc84488ed6f558885df69d2ccc64428b69
Peter Lombaers <peter@idfuse.nl>
2024-10-31 12:06:57 +0100

    Move writing test to `test_writers`

--- tests/test_readers.py
+++ tests/test_readers.py
@@ -167,19 +167,6 @@ def test_nan_values_csv():
     assert records[2].doi is None
 
 
-def test_write_data(tmpdir):
-    fp_in = Path("tests", "demo_data", "generic_labels.csv")
-    fp_out = Path(tmpdir, "generic_out.csv")
-    asr_data = load_dataset(fp_in)
-    asr_data.to_file(fp_out, labels=[[0, 0], [2, 1], [3, 1]])
-
-    tmp_csv_fp_out = Path(tmpdir, "tmp_generic_labels.csv")
-    asr_data.to_file(tmp_csv_fp_out)
-    asr_data_diff = load_dataset(tmp_csv_fp_out)
-    # Check if export file includes labels [1,0]
-    assert list(asr_data["included"]) == list(asr_data_diff["included"])
-
-
 @mark.internet_required
 def test_load_dataset_from_url(tmpdir):
     url = "https://zenodo.org/api/records/1162952/files/Hall.csv/content"
--- tests/test_writers.py
+++ tests/test_writers.py
@@ -5,6 +5,19 @@ from pytest import mark
 from asreview import load_dataset
 
 
+def test_write_data(tmpdir):
+    fp_in = Path("tests", "demo_data", "generic_labels.csv")
+    fp_out = Path(tmpdir, "generic_out.csv")
+    asr_data = load_dataset(fp_in)
+    asr_data.to_file(fp_out, labels=[[0, 0], [2, 1], [3, 1]])
+
+    tmp_csv_fp_out = Path(tmpdir, "tmp_generic_labels.csv")
+    asr_data.to_file(tmp_csv_fp_out)
+    asr_data_diff = load_dataset(tmp_csv_fp_out)
+    # Check if export file includes labels [1,0]
+    assert list(asr_data["included"]) == list(asr_data_diff["included"])
+
+
 @mark.parametrize("test_file", [("baseline_tag-notes_labels.ris")])
 def test_asreview_labels_ris(test_file, tmpdir):
     fp_in = Path("tests", "demo_data", test_file)
