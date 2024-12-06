1c266b64ab5c3a824e382c9a34f28b3cf1b6f81b
Jonathan de Bruin <jonathandebruinos@gmail.com>
2024-12-06 11:40:04 +0100

    Simplify ranking table replacement

--- asreview/state/sqlstate.py
+++ asreview/state/sqlstate.py
@@ -225,12 +225,9 @@ class SQLiteState:
                 f"{list(RANKING_TABLE_COLUMNS_PANDAS_DTYPES.keys())}."
             )
 
-        cur = self._conn.cursor()
-        cur.execute("delete from last_ranking")
-        self._conn.commit()
-        cur.close()
-
-        last_ranking.to_sql("last_ranking", self._conn, if_exists="append", index=False)
+        last_ranking.to_sql(
+            "last_ranking", self._conn, if_exists="replace", index=False
+        )
 
     def add_last_ranking(
         self,
