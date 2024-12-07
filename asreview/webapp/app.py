85576518c820af3478719421a62de57523f145d5
Casper Kaandorp <casper@compunist.nl>
2024-12-07 07:05:29 +0100

    routes

--- asreview/webapp/app.py
+++ asreview/webapp/app.py
@@ -129,7 +129,11 @@ def create_app(config_path=None):
         logging.error(e.original_exception)
         return jsonify(message=str(e.original_exception)), 500
 
+    
+    @app.route("/confirm_account", methods=["GET"])
+    @app.route("/forgot_password", methods=["GET"])
     @app.route("/signin", methods=["GET"])
+    @app.route("/signup", methods=["GET"])
     @app.route("/oauth_callback", methods=["GET"])
     @app.route("/reset_password", methods=["GET"])
     def index(**kwargs):
