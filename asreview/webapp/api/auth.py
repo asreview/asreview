b0337e3d39fc835ce6b9d0c0ed4d72bb7362bbce
Dawa Ometto <d.l.a.ometto1@uu.nl>
2024-12-11 15:55:27 +0100

    Fix merge

--- asreview/webapp/api/auth.py
+++ asreview/webapp/api/auth.py
@@ -39,68 +39,6 @@ from asreview.webapp.authentication.utils import send_confirm_account_email
 
 bp = Blueprint("auth", __name__, url_prefix="/auth")
 
-
-def _has_email_configuration(app):
-    return all(
-        [
-            app.config.get("MAIL_SERVER", False),
-            app.config.get("MAIL_USERNAME", False),
-            app.config.get("MAIL_PASSWORD", False),
-        ]
-    )
-
-
-def perform_login_user(user):
-    """Helper function to login a user"""
-    return login_user(user, remember=True, duration=dt.timedelta(days=31))
-
-
-# TODO: not sure if this file is the right place for this function
-def send_forgot_password_email(user, cur_app):
-    # do not send email in test environment
-    if not cur_app.testing:
-        # set name
-        name = user.name or "ASReview user"
-        # create a mailer
-        mailer = Mail(cur_app)
-        # open templates as string and render
-        root_path = Path(cur_app.root_path)
-        with open(root_path / "templates" / "emails" / "forgot_password.html") as f:
-            html_text = render_template_string(f.read(), name=name, token=user.token)
-        with open(root_path / "templates" / "emails" / "forgot_password.txt") as f:
-            txt_text = render_template_string(f.read(), name=name, token=user.token)
-        # create message
-        msg = Message("ASReview: forgot password", recipients=[user.email])
-        msg.body = txt_text
-        msg.html = html_text
-        return mailer.send(msg)
-
-
-# TODO: not sure if this file is the right place for this function
-def send_confirm_account_email(user, cur_app, email_type="create"):
-    # do not send email in test environment
-    if not cur_app.testing:
-        # get necessary information out of user object
-        name = user.name or "ASReview user"
-        # create a mailer
-        mailer = Mail(cur_app)
-        # open templates as string and render
-        root_path = Path(cur_app.root_path)
-        with open(root_path / "templates" / "emails" / "confirm_account.html") as f:
-            html_text = render_template_string(
-                f.read(), name=name, token=user.token, email_type=email_type
-            )
-        with open(root_path / "templates" / "emails" / "confirm_account.txt") as f:
-            txt_text = render_template_string(
-                f.read(), name=name, token=user.token, email_type=email_type
-            )
-        # create message
-        msg = Message("ASReview: please confirm your account", recipients=[user.email])
-        msg.body = txt_text
-        msg.html = html_text
-        return mailer.send(msg)
-
-
 # ------------------
 #      ROUTES
 # ------------------
