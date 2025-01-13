29cb7e1d39d78213703625e355f8f6e0cacee0f8
Jonathan de Bruin <jonathandebruinos@gmail.com>
2025-01-13 17:20:37 +0100

    Add sklearn intersphinx mapping

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -280,6 +280,7 @@ intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
     "numpy": ("https://numpy.org/doc/stable/", None),
     "pandas": ("https://pandas.pydata.org/docs/", None),
+    "sklearn": ("https://scikit-learn.org/stable", None),
 }
 
 # -- Napoleon options --------------------------------------------------------
