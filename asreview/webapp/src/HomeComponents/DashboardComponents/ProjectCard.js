a2c3797a7e5ea95e6d9c0f8f71d842d7b73cb852
Jonathan de Bruin <jonathandebruinos@gmail.com>
2025-01-22 11:53:57 +0100

    Add ReviewDone icon to ProjectCard if finished

--- asreview/webapp/src/HomeComponents/DashboardComponents/ProjectCard.js
+++ asreview/webapp/src/HomeComponents/DashboardComponents/ProjectCard.js
@@ -1,6 +1,7 @@
 import {
   DeleteForeverOutlined,
   DoneAllOutlined,
+  RemoveDoneOutlined,
   DownloadOutlined,
   GroupAdd,
   MoreHoriz,
@@ -228,7 +229,11 @@ const ProjectCard = ({ project, mode, showSimulatingSpinner = true }) => {
                     }
                   >
                     <ListItemIcon>
-                      <DoneAllOutlined />
+                      {review?.status === projectStatuses.REVIEW ? (
+                        <DoneAllOutlined />
+                      ) : (
+                        <RemoveDoneOutlined />
+                      )}
                     </ListItemIcon>
                     <ListItemText>
                       {review?.status === projectStatuses.REVIEW
