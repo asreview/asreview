02238329a9736869ad143877522aff4c70818bc8
Jonathan de Bruin <jonathandebruinos@gmail.com>
2025-02-18 12:26:54 +0100

    Add migrate tool for version 1 to 2 and display warnings on import (#2079)

--- tests/test_migrate_project.py
+++ tests/test_migrate_project.py
@@ -1,7 +1,11 @@
 import json
 from pathlib import Path
 
+import jsonschema
+import pandas
+
 import asreview as asr
+from asreview.project.schema import SCHEMA
 
 
 def test_project_migration_1_to_2(tmpdir):
@@ -14,15 +18,25 @@ def test_project_migration_1_to_2(tmpdir):
         "asreview-project-v1-5-startreview.asreview",
     )
 
+    assert asreview_v1_file.exists()
+
     project = asr.Project.load(open(asreview_v1_file, "rb"), tmpdir, safe_import=True)
 
     assert project.config["version"].startswith("2.")
+    jsonschema.validate(instance=project.config, schema=SCHEMA)
 
+    # test state
     with asr.open_state(project) as state:
         state.get_results_table()
         state.get_last_ranking_table()
         state.get_decision_changes()
 
+    data = project.data_store
+
+    assert isinstance(data["title"], pandas.Series)
+    assert isinstance(data["included"], pandas.Series)
+
+    # test model settings
     with open(
         Path(
             project.project_path,
