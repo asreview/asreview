bad0317fa8a5064bd1749c0cd75310596fe82103
Jonathan de Bruin <jonathandebruinos@gmail.com>
2025-04-06 17:17:51 +0200

    Happy lint

--- asreview/webapp/src/constants/theme.js
+++ asreview/webapp/src/constants/theme.js
@@ -21,7 +21,6 @@
 // - The background color is a light beige color which is used
 // for the background of the app.
 
-import { Alert } from "@mui/material";
 import { alpha, getContrastRatio } from "@mui/material/styles";
 
 export const theme = {
