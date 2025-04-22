f4462ef827eb77eff7c448b015e44d14376a7872
Dawa Ometto <d.l.a.ometto1@uu.nl>
2025-04-22 14:38:08 +0200

    Add integration test workflow

--- asreview/webapp/tests/integration_tests/conftest.py
+++ asreview/webapp/tests/integration_tests/conftest.py
@@ -1,8 +1,18 @@
+from os import getenv
+
+import pytest
+
 def pytest_addoption(parser):
     parser.addoption("--url", action="store", default="http://localhost:3000/")
     parser.addoption("--database-uri", action="store")
     parser.addoption("--reading-time", action="store", type=int, default=5)
 
+@pytest.fixture
+def chrome_options(chrome_options):
+    if bool(getenv('CI', False)):
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument("--no-sandbox")
+    return chrome_options
 
 @pytest.fixture(scope="session")
 def url(pytestconfig):
