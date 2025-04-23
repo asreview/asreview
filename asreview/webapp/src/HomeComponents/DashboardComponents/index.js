b79b57ec84205737fca3980739f3e30755807a52
Jonathan de Bruin <jonathandebruinos@gmail.com>
2025-04-23 13:13:01 +0200

    Remove old project conversion dialog

--- asreview/webapp/src/HomeComponents/DashboardComponents/index.js
+++ asreview/webapp/src/HomeComponents/DashboardComponents/index.js
@@ -1,6 +1,5 @@
 export { default as DashboardPageHeader } from "./DashboardPageHeader";
 export { default as ProfilePage } from "./ProfilePage";
-export { default as ProjectCheckDialog } from "./ProjectCheckDialog";
 export { default as ProjectCard } from "./ProjectCard";
 export { default as ProjectsOverview } from "./ProjectsOverview";
 export { default as NewProjectButton } from "./NewProjectButton";
