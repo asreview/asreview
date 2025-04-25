b10aab93780b0553e42b4d11b74fcff06a8dc830
timovdk <5330531+timovdk@users.noreply.github.com>
2025-04-25 14:43:25 +0200

    Add links to optimization tags

--- asreview/models/models.py
+++ asreview/models/models.py
@@ -19,6 +19,7 @@ AI_MODEL_CONFIGURATIONS = [
         # ELAS u4 is the default model for this version ASReview LAB. The model
         # parameters have been optimized on the SYNERGY dataset. After expert
         # elicitation, the model and parameters have been chosen.
+        # Optimization setup: https://github.com/asreview/asreview-optuna/releases/tag/ASReview2_0b4-nb-tfidf-full-1
         "name": "elas_u4",
         "label": "ELAS u4",
         "type": "ultra",
@@ -59,6 +60,10 @@ AI_MODEL_CONFIGURATIONS = [
     # ELAS u1 refers to the model that was used in the ASReview LAB version 0. The
     # model is not available in the current version of ASReview LAB.
     {
+        # ELAS l2 is the multilingual model for this version ASReview LAB. The model
+        # parameters have been optimized on the SYNERGY dataset. After expert
+        # elicitation, the model and parameters have been chosen.
+        # Optimization setup: https://github.com/asreview/asreview-optuna/releases/tag/ASReview2_0b4-svm-e5-full-1
         "name": "elas_l2",
         "label": "ELAS l2",
         "type": "lang",
@@ -74,6 +79,10 @@ AI_MODEL_CONFIGURATIONS = [
         ),
     },
     {
+        # ELAS h3 is the heavy model for this version ASReview LAB. The model
+        # parameters have been optimized on the SYNERGY dataset. After expert
+        # elicitation, the model and parameters have been chosen.
+        # Optimization setup: https://github.com/asreview/asreview-optuna/releases/tag/ASReview2_0b4-svm-mxbai-full-1
         "name": "elas_h3",
         "label": "ELAS h3",
         "type": "heavy",
