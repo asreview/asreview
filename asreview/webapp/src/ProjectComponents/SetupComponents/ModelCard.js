bd937efc1418f0612d1451db65bf41805996911d
J535D165 <J535D165@users.noreply.github.com>
2025-05-12 08:30:58 +0000

    Prettified Code!

--- asreview/webapp/src/ProjectComponents/SetupComponents/ModelCard.js
+++ asreview/webapp/src/ProjectComponents/SetupComponents/ModelCard.js
@@ -220,11 +220,10 @@ const ModelCard = ({ mode = null, trainNewModel = false }) => {
                         <>
                           This AI model trains in the background and takes
                           substantially longer to vectorize the dataset than the
-                          faster <strong>Ultra</strong> models. While it
-                          trains the model, you can continue
-                          screening with the previous model. The system will
-                          automatically switch to the new model as soon as it's
-                          ready.
+                          faster <strong>Ultra</strong> models. While it trains
+                          the model, you can continue screening with the
+                          previous model. The system will automatically switch
+                          to the new model as soon as it's ready.
                         </>
                       }
                     </Alert>
