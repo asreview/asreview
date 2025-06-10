f9335f1f15055294c2e57e3dfacdfaeb3bbc038e
Jonathan de Bruin <jonathandebruinos@gmail.com>
2025-06-10 09:21:09 +0200

    Add offline page with start instructions to PWA (#2235)

--- asreview/webapp/src/serviceWorker.js
+++ asreview/webapp/src/serviceWorker.js
@@ -22,42 +22,38 @@ const isLocalhost = Boolean(
 
 export function register(config) {
   if (process.env.NODE_ENV === "production" && "serviceWorker" in navigator) {
-    // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
-      // Our service worker won't work if PUBLIC_URL is on a different origin
-      // from what our page is served on. This might happen if a CDN is used to
-      // serve assets; see https://github.com/facebook/create-react-app/issues/2374
+      return;
+    }
+
+    if (!isLocalhost) {
+      // We only use offline pages on localhost
       return;
     }
 
     window.addEventListener("load", () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      registerValidSW(config);
+    });
+  }
+}
 
-      if (isLocalhost) {
-        // This is running on localhost. Let's check if a service worker still exists or not.
-        checkValidServiceWorker(swUrl, config);
-
-        // Add some additional logging to localhost, pointing developers to the
-        // service worker/PWA documentation.
-        navigator.serviceWorker.ready.then(() => {
-          console.log(
-            "This web app is being served cache-first by a service " +
-              "worker. To learn more, visit https://bit.ly/CRA-PWA",
-          );
-        });
-      } else {
-        // Is not localhost. Just register service worker
-        registerValidSW(swUrl, config);
+function injectOfflinePage() {
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.getRegistration().then((registration) => {
+      if (registration && registration.active) {
+        registration.active.postMessage({ type: "CACHE_OFFLINE_PAGE" });
       }
     });
   }
 }
 
-function registerValidSW(swUrl, config) {
+function registerValidSW(config) {
   navigator.serviceWorker
-    .register(swUrl)
+    .register(`${process.env.PUBLIC_URL}/service-worker.js`)
     .then((registration) => {
+      // Inject offline page caching after registration
+      injectOfflinePage();
       registration.onupdatefound = () => {
         const installingWorker = registration.installing;
         if (installingWorker == null) {
@@ -98,32 +94,13 @@ function registerValidSW(swUrl, config) {
     });
 }
 
-function checkValidServiceWorker(swUrl, config) {
-  // Check if the service worker can be found. If it can't reload the page.
-  fetch(swUrl)
-    .then((response) => {
-      // Ensure service worker exists, and that we really are getting a JS file.
-      const contentType = response.headers.get("content-type");
-      if (
-        response.status === 404 ||
-        (contentType != null && contentType.indexOf("javascript") === -1)
-      ) {
-        // No service worker found. Probably a different app. Reload the page.
-        navigator.serviceWorker.ready.then((registration) => {
-          registration.unregister().then(() => {
-            window.location.reload();
-          });
-        });
-      } else {
-        // Service worker found. Proceed as normal.
-        registerValidSW(swUrl, config);
-      }
-    })
-    .catch(() => {
-      console.log(
-        "No internet connection found. App is running in offline mode.",
-      );
-    });
+// Add a message event listener to handle offline page caching in the service worker
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.addEventListener("message", (event) => {
+    if (event.data && event.data.type === "CACHE_OFFLINE_PAGE") {
+      // No-op in client, handled in service worker file
+    }
+  });
 }
 
 export function unregister() {
