d68d972855839a70878ed35a527ef620a1f1b33b
Jiun Kim <kkimj@hanyang.ac.kr>
2025-09-23 16:01:20 +0900

    refactor: Handle empty str or null w/ journal/source

--- asreview/webapp/src/ProjectComponents/ReviewComponents/RecordCard.js
+++ asreview/webapp/src/ProjectComponents/ReviewComponents/RecordCard.js
@@ -53,26 +53,29 @@ const RecordCardContent = ({ record, fontSize, collapseAbstract }) => {
             </Box>
           )}
         </Typography>
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-          }}
-        >
-          {record.journal ? (
-            <Typography sx={{ color: "text.secondary", fontWeight: "bold" }}>
-              {record.journal}
-            </Typography>
-          ) : (
-            <Box sx={{ flexGrow: 1 }} />
-          )}
-          {record.source && (
-            <Typography variant="body1" sx={{ color: "text.secondary" }}>
-              {record.source}
-            </Typography>
-          )}
-        </Box>
+        {!(
+          (record.journal === "" || record.journal === null) &&
+          (record.source === "" || record.source === null)
+        ) && (
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            {!(record.journal === "" || record.journal === null) && (
+              <Typography sx={{ color: "text.secondary", fontWeight: "bold" }}>
+                {record.journal}
+              </Typography>
+            )}
+            {!(record.source === "" || record.source === null) && (
+              <Typography variant="body1" sx={{ color: "text.secondary" }}>
+                {record.source}
+              </Typography>
+            )}
+          </Box>
+        )}
         <Divider />
         <Stack direction="row" spacing={1}>
           {!(record.doi === undefined || record.doi === null) && (
